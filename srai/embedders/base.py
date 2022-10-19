841815a41f936e21081aa0b4d5f9dd4e20374704
Piotr Gramacki <p.gramacki@gmail.com>
2022-10-20 00:23:43 +0200

    chore: update embed return type to numpy array

--- srai/embedders/base.py
+++ srai/embedders/base.py
@@ -4,19 +4,21 @@ import abc
 from typing import Any
 
 import geopandas as gpd
+import numpy as np
+import numpy.typing as npt
 
 
 class BaseEmbedder(abc.ABC):
     """Base abstract class for embedders."""
 
     @abc.abstractmethod
-    def embed(self, gdf: gpd.GeoDataFrame, **kwargs: Any) -> gpd.GeoDataFrame:
+    def embed(self, gdf: gpd.GeoDataFrame, **kwargs: Any) -> npt.NDArray[np.float64]:
         """Embed a given GeoDataFrame.
 
         Args:
             gdf: GeoDataFrame to be embedded.
 
         Returns:
-            GeoDataFrame with the embedded data.
+            np.ndarray with embedding for each row.
         """
         raise NotImplementedError
