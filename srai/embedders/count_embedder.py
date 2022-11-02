8990a1816daf8e46db88c39162ebff6b440967d4
Szymon Woźniak <swozniak6@gmail.com>
2022-11-02 21:11:28 +0100

    chore: rewrite docstring for embed in CountEmbedder

--- srai/embedders/count_embedder.py
+++ srai/embedders/count_embedder.py
@@ -12,19 +12,32 @@ class CountEmbedder:
     """Simple Embedder that counts features."""
 
     def embed(
-        self, regions_gdf: gpd.GeoDataFrame, features_gdf: gpd.GeoDataFrame, joint: gpd.GeoDataFrame
+        self,
+        regions_gdf: gpd.GeoDataFrame,
+        features_gdf: gpd.GeoDataFrame,
+        joint_gdf: gpd.GeoDataFrame,
     ) -> gpd.GeoDataFrame:
         """
-        Embed a given GeoDataFrame by counting features.
+        Embed a given GeoDataFrame.
+
+        Creates region embeddings by counting the frequencies of each feature value.
+        Expects features_gdf to be in wide format with each column
+        being a separate type of feature (e.g. amenity, leisure)
+        and rows holding values of these features for each object.
+        The resulting GeoDataFrame will have columns made by combining
+        the feature name (column) and value (row) e.g. amenity_fuel or type_0.
+        The rows will hold numbers of this type of feature in each region.
 
         Args:
-            gdf (gpd.GeoDataFrame): GeoDataFrame to be embedded.
+            regions_gdf (gpd.GeoDataFrame): Region indexes and geometries.
+            features_gdf (gpd.GeoDataFrame): Feature indexes, geometries and feature values.
+            joint_gdf (gpd.GeoDataFrame): Joiner result with region-feature multi-index.
 
         Returns:
             gpd.GeoDataFrame with embedding for each region.
 
         """
-        joint_with_features = joint.join(features_gdf.drop("geometry", axis=1))
+        joint_with_features = joint_gdf.join(features_gdf.drop("geometry", axis=1))
         region_embeddings = (
             pd.get_dummies(joint_with_features.drop("geometry", axis=1)).groupby(level=0).sum()
         )
