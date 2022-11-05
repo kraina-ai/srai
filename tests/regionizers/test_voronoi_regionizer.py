aa729bf820e1efec8187af5040b16c5143274e5f
Kamil Raczycki <raczyckikamil@gmail.com>
2022-11-05 08:57:00 +0100

    fix: removed duplicated code

--- tests/regionizers/test_voronoi_regionizer.py
+++ tests/regionizers/test_voronoi_regionizer.py
@@ -10,6 +10,25 @@ bbox = box(minx=-180, maxx=180, miny=-90, maxy=90)
 bbox_gdf = gpd.GeoDataFrame({"geometry": [bbox]}, crs="EPSG:4326")
 
 
+def _get_basic_voronoi_regionizer() -> VoronoiRegionizer:
+    seeds_gdf = gpd.GeoDataFrame(
+        {
+            "geometry": [
+                Point(0, 0),
+                Point(90, 0),
+                Point(180, 0),
+                Point(-90, 0),
+                Point(0, 90),
+                Point(0, -90),
+            ]
+        },
+        index=[1, 2, 3, 4, 5, 6],
+        crs="EPSG:4326",
+    )
+    vr = VoronoiRegionizer(seeds=seeds_gdf)
+    return vr
+
+
 def test_zero_seeds_value_error() -> None:
     """Test checks if zero seeds is disallowed."""
     with pytest.raises(ValueError):
@@ -48,21 +67,7 @@ def test_single_seed_region() -> None:
 
 def test_multiple_seeds_regions() -> None:
     """Test checks if regions are generated correctly."""
-    seeds_gdf = gpd.GeoDataFrame(
-        {
-            "geometry": [
-                Point(0, 0),
-                Point(90, 0),
-                Point(180, 0),
-                Point(-90, 0),
-                Point(0, 90),
-                Point(0, -90),
-            ]
-        },
-        index=[1, 2, 3, 4, 5, 6],
-        crs="EPSG:4326",
-    )
-    vr = VoronoiRegionizer(seeds=seeds_gdf)
+    vr = _get_basic_voronoi_regionizer()
     result_gdf = vr.transform(gdf=bbox_gdf)
     assert len(result_gdf.index) == 6
     assert _merge_disjointed_gdf_geometries(result_gdf).difference(bbox).is_empty
@@ -90,21 +95,7 @@ def test_four_close_seed_region() -> None:
 
 def test_default_parameter() -> None:
     """Test checks if regions are generated correctly with a default mask."""
-    seeds_gdf = gpd.GeoDataFrame(
-        {
-            "geometry": [
-                Point(0, 0),
-                Point(90, 0),
-                Point(180, 0),
-                Point(-90, 0),
-                Point(0, 90),
-                Point(0, -90),
-            ]
-        },
-        index=[1, 2, 3, 4, 5, 6],
-        crs="EPSG:4326",
-    )
-    vr = VoronoiRegionizer(seeds=seeds_gdf)
+    vr = _get_basic_voronoi_regionizer()
     result_gdf = vr.transform(gdf=None)
     assert len(result_gdf.index) == 6
     assert _merge_disjointed_gdf_geometries(result_gdf).difference(bbox).is_empty
@@ -116,21 +107,7 @@ def test_clipping_parameter() -> None:
     area = world[world.name == "Poland"]
     area = area.to_crs(epsg=4326)
 
-    seeds_gdf = gpd.GeoDataFrame(
-        {
-            "geometry": [
-                Point(0, 0),
-                Point(90, 0),
-                Point(180, 0),
-                Point(-90, 0),
-                Point(0, 90),
-                Point(0, -90),
-            ]
-        },
-        index=[1, 2, 3, 4, 5, 6],
-        crs="EPSG:4326",
-    )
-    vr = VoronoiRegionizer(seeds=seeds_gdf)
+    vr = _get_basic_voronoi_regionizer()
     result_gdf = vr.transform(gdf=area)
     assert len(result_gdf.index) == 1
     assert _merge_disjointed_gdf_geometries(result_gdf).difference(area.iloc[0].geometry).is_empty
