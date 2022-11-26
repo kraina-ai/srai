65d6d995ec763cec48a182a20ec57d522975c84c
Piotr Gramacki <p.gramacki@gmail.com>
2022-11-27 00:11:30 +0100

    chore: update base Joiner class

--- srai/joiners/base.py
+++ srai/joiners/base.py
@@ -9,16 +9,19 @@ class BaseJoiner(abc.ABC):
     """Base abstract class for joiners."""
 
     @abc.abstractmethod
-    def join(self, regions: gpd.GeoDataFrame, features: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    def join(
+        self, regions: gpd.GeoDataFrame, features: gpd.GeoDataFrame, return_geom: bool = True
+    ) -> gpd.GeoDataFrame:
         """
         Join features to regions.
 
         Args:
             regions (gpd.GeoDataFrame): regions with which features are joined
             features (gpd.GeoDataFrame): features to be joined
+            return_geom (bool): whether to return geometry of the joined features
         Returns:
             GeoDataFrame with an intersection of regions and features, which contains
-            a MultiIndex and a geometry with the intersection
+            a MultiIndex and optionally a geometry with the intersection
 
         """
         raise NotImplementedError
