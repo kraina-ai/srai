d714d1181e78462421abf5a13da5daa6fd56f512
Kamil Raczycki <raczyckikamil@gmail.com>
2022-11-29 22:40:39 +0100

    fix: changed error from voronoi regionizer test

--- tests/regionizers/test_voronoi_regionizer.py
+++ tests/regionizers/test_voronoi_regionizer.py
@@ -11,7 +11,7 @@ from srai.utils import _merge_disjointed_gdf_geometries
 
 def test_empty_gdf_value_error(gdf_empty) -> None:  # type: ignore
     """Test checks if empty GeoDataFrames are disallowed."""
-    with pytest.raises(ValueError):
+    with pytest.raises(AttributeError):
         VoronoiRegionizer(seeds=gdf_empty)
 
 
