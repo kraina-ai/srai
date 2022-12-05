afea30f10d350d72711d86ec68b946647d18e4a6
Szymon Woźniak <36135334+simonusher@users.noreply.github.com>
2022-12-05 13:54:38 +0100

    feat: add CountEmbedder (#77)
    * feat: add  CountEmbedder

---
+++ srai/embedders/count_embedder.py
@@ -0,0 +1,145 @@
+"""
+Count Embedder.
+
+This module contains count embedder implementation.
+
+"""
+from typing import List, Optional
+
+import geopandas as gpd
+import pandas as pd
+
+
+class CountEmbedder:
+    """Simple Embedder that counts occurences of feature values."""
+
+    def __init__(self, expected_output_features: Optional[List[str]] = None) -> None:
+        """
+        Init CountEmbedder.
+
+        Args:
+            expected_output_features (List[str], optional): The features that are expected
+                to be found in the resulting embedding. If not None, the missing features are added
+                and filled with 0. The unexpected features are removed.
+                The resulting columns are sorted accordingly. Defaults to None.
+
+        """
+        if expected_output_features is not None:
+            self.expected_output_features = pd.Series(expected_output_features)
+        else:
+            self.expected_output_features = None
+
+    def embed(
+        self,
+        regions_gdf: gpd.GeoDataFrame,
+        features_gdf: gpd.GeoDataFrame,
+        joint_gdf: gpd.GeoDataFrame,
+    ) -> pd.DataFrame:
+        """
+        Embed a given GeoDataFrame.
+
+        Creates region embeddings by counting the frequencies of each feature value.
+        Expects features_gdf to be in wide format with each column
+        being a separate type of feature (e.g. amenity, leisure)
+        and rows to hold values of these features for each object.
+        The resulting GeoDataFrame will have columns made by combining
+        the feature name (column) and value (row) e.g. amenity_fuel or type_0.
+        The rows will hold numbers of this type of feature in each region.
+
+        Args:
+            regions_gdf (gpd.GeoDataFrame): Region indexes and geometries.
+            features_gdf (gpd.GeoDataFrame): Feature indexes, geometries and feature values.
+            joint_gdf (gpd.GeoDataFrame): Joiner result with region-feature multi-index.
+
+        Returns:
+            pd.DataFrame: Embedding and geometry index for each region in regions_gdf.
+
+        Raises:
+            ValueError: If features_gdf is empty and self.expected_output_features is not set.
+            ValueError: If any of the gdfs index names is None.
+            ValueError: If joint_gdf.index is not of type pd.MultiIndex or doesn't have 2 levels.
+            ValueError: If index levels in gdfs don't overlap correctly.
+
+        """
+        self._validate_indexes(regions_gdf, features_gdf, joint_gdf)
+        if features_gdf.empty:
+            if self.expected_output_features is not None:
+                return pd.DataFrame(
+                    0, index=regions_gdf.index, columns=self.expected_output_features
+                )
+            else:
+                raise ValueError(
+                    "Cannot embed with empty features_gdf and no expected_output_features."
+                )
+
+        regions_df = self._remove_geometry_if_present(regions_gdf)
+        features_df = self._remove_geometry_if_present(features_gdf)
+        joint_df = self._remove_geometry_if_present(joint_gdf)
+
+        feature_encodings = pd.get_dummies(features_df)
+        joint_with_encodings = joint_df.join(feature_encodings)
+        region_embeddings = joint_with_encodings.groupby(level=0).sum()
+
+        if self.expected_output_features is not None:
+            region_embeddings = self._filter_to_expected_features(region_embeddings)
+        region_embedding_df = regions_df.join(region_embeddings, how="left").fillna(0).astype(int)
+
+        return region_embedding_df
+
+    def _validate_indexes(
+        self,
+        regions_gdf: gpd.GeoDataFrame,
+        features_gdf: gpd.GeoDataFrame,
+        joint_gdf: gpd.GeoDataFrame,
+    ) -> None:
+        if regions_gdf.index.name is None:
+            raise ValueError("regions_gdf must have a named index.")
+
+        if features_gdf.index.name is None:
+            raise ValueError("features_gdf must have a named index.")
+
+        if not isinstance(joint_gdf.index, pd.MultiIndex):
+            raise ValueError(
+                f"joint_gdf.index must be of type pandas.MultiIndex, not {type(joint_gdf.index)}"
+            )
+
+        print(joint_gdf.index.names)
+        if len(joint_gdf.index.names) != 2:
+            raise ValueError(
+                f"joint_gdf.index must have 2 levels, has {len(joint_gdf.index.names)}"
+            )
+
+        if regions_gdf.index.name != joint_gdf.index.names[0]:
+            raise ValueError(
+                f"Name of regions_gdf.index ({regions_gdf.index.name}) must be equal to the name of"
+                f" the 1st level of joint_gdf.index ({joint_gdf.index.names[0]})"
+            )
+
+        if features_gdf.index.name != joint_gdf.index.names[1]:
+            raise ValueError(
+                f"Name of features_gdf.index ({features_gdf.index.name}) must be equal to the name"
+                f" of the 2nd level of joint_gdf.index ({joint_gdf.index.names[1]})"
+            )
+
+    def _filter_to_expected_features(self, region_embeddings: pd.DataFrame) -> pd.DataFrame:
+        """
+        Add missing and remove excessive columns from embeddings.
+
+        Args:
+            region_embeddings (pd.DataFrame): Counted frequencies of each feature value.
+
+        Returns:
+            pd.DataFrame: Embeddings with expected columns only.
+
+        """
+        missing_features = self.expected_output_features[
+            ~self.expected_output_features.isin(region_embeddings.columns)
+        ]
+        region_embeddings[missing_features] = 0
+        region_embeddings = region_embeddings[self.expected_output_features]
+        return region_embeddings
+
+    def _remove_geometry_if_present(self, data: gpd.GeoDataFrame) -> pd.DataFrame:
+        if "geometry" in data.columns:
+            data = data.drop(columns="geometry")
+        return pd.DataFrame(data)
---
+++ tests/embedders/test_count_embedder.py
@@ -0,0 +1,193 @@
+"""CountEmbedder tests."""
+from contextlib import nullcontext as does_not_raise
+from typing import Any, List, Union
+
+import geopandas as gpd
+import pandas as pd
+import pytest
+from pandas.testing import assert_frame_equal, assert_index_equal
+
+from srai.embedders import CountEmbedder
+
+
+@pytest.fixture  # type: ignore
+def expected_embedding_df() -> pd.DataFrame:
+    """Get expected CountEmbedder output for the default case."""
+    expected_df = pd.DataFrame(
+        {
+            "region_id": ["891e2040897ffff", "891e2040d4bffff", "891e2040d5bffff"],
+            "leisure_adult_gaming_centre": [0, 0, 1],
+            "leisure_playground": [0, 1, 0],
+            "amenity_pub": [1, 0, 1],
+        },
+    )
+    expected_df.set_index("region_id", inplace=True)
+
+    return expected_df
+
+
+@pytest.fixture  # type: ignore
+def expected_feature_names() -> List[str]:
+    """Get expected feature names for CountEmbedder."""
+    expected_feature_names = ["amenity_parking", "leisure_park", "amenity_pub"]
+    return expected_feature_names
+
+
+@pytest.fixture  # type: ignore
+def specified_features_expected_embedding_df() -> pd.DataFrame:
+    """Get expected CountEmbedder output for the case with specified features."""
+    expected_df = pd.DataFrame(
+        {
+            "region_id": ["891e2040897ffff", "891e2040d4bffff", "891e2040d5bffff"],
+            "amenity_parking": [0, 0, 0],
+            "leisure_park": [0, 0, 0],
+            "amenity_pub": [1, 0, 1],
+        },
+    )
+    expected_df.set_index("region_id", inplace=True)
+
+    return expected_df
+
+
+def test_correct_embedding(
+    gdf_regions: gpd.GeoDataFrame,
+    gdf_features: gpd.GeoDataFrame,
+    gdf_joint: gpd.GeoDataFrame,
+    expected_embedding_df: pd.DataFrame,
+) -> None:
+    """Test if CountEmbedder returns correct result in the default case."""
+    embedding_df = CountEmbedder().embed(
+        regions_gdf=gdf_regions, features_gdf=gdf_features, joint_gdf=gdf_joint
+    )
+    assert_frame_equal(embedding_df, expected_embedding_df, check_dtype=False)
+
+
+def test_correct_embedding_expected_features(
+    gdf_regions: gpd.GeoDataFrame,
+    gdf_features: gpd.GeoDataFrame,
+    gdf_joint: gpd.GeoDataFrame,
+    expected_feature_names: List[str],
+    specified_features_expected_embedding_df: pd.DataFrame,
+) -> None:
+    """Test if CountEmbedder returns correct result in the specified features case."""
+    embedding_df = CountEmbedder(expected_output_features=expected_feature_names).embed(
+        regions_gdf=gdf_regions, features_gdf=gdf_features, joint_gdf=gdf_joint
+    )
+    assert_frame_equal(embedding_df, specified_features_expected_embedding_df, check_dtype=False)
+
+
+@pytest.mark.parametrize(  # type: ignore
+    "regions_fixture,features_fixture,joint_fixture,expected_features_fixture,expectation",
+    [
+        (
+            "gdf_regions_empty",
+            "gdf_features",
+            "gdf_joint",
+            None,
+            does_not_raise(),
+        ),
+        (
+            "gdf_regions",
+            "gdf_features_empty",
+            "gdf_joint",
+            None,
+            pytest.raises(ValueError),
+        ),
+        (
+            "gdf_regions",
+            "gdf_features_empty",
+            "gdf_joint",
+            "expected_feature_names",
+            does_not_raise(),
+        ),
+        (
+            "gdf_regions",
+            "gdf_features",
+            "gdf_joint_empty",
+            None,
+            does_not_raise(),
+        ),
+    ],
+)
+def test_empty(
+    regions_fixture: str,
+    features_fixture: str,
+    joint_fixture: str,
+    expected_features_fixture: Union[str, None],
+    expectation: Any,
+    request: Any,
+) -> None:
+    """Test CountEmbedder handling of empty input data frames."""
+    expected_output_features = (
+        None
+        if expected_features_fixture is None
+        else request.getfixturevalue(expected_features_fixture)
+    )
+    embedder = CountEmbedder(expected_output_features)
+    gdf_regions: gpd.GeoDataFrame = request.getfixturevalue(regions_fixture)
+    gdf_features: gpd.GeoDataFrame = request.getfixturevalue(features_fixture)
+    gdf_joint: gpd.GeoDataFrame = request.getfixturevalue(joint_fixture)
+
+    with expectation:
+        embedding = embedder.embed(gdf_regions, gdf_features, gdf_joint)
+        assert len(embedding) == len(gdf_regions)
+        assert_index_equal(embedding.index, gdf_regions.index)
+        if expected_output_features:
+            assert embedding.columns.tolist() == expected_output_features
+
+        assert (embedding == 0).all().all()
+
+
+@pytest.mark.parametrize(  # type: ignore
+    "regions_fixture,features_fixture,joint_fixture,expectation",
+    [
+        (
+            "gdf_unnamed_single_index",
+            "gdf_features",
+            "gdf_joint",
+            pytest.raises(ValueError),
+        ),
+        (
+            "gdf_regions",
+            "gdf_unnamed_single_index",
+            "gdf_joint",
+            pytest.raises(ValueError),
+        ),
+        (
+            "gdf_regions",
+            "gdf_features",
+            "gdf_unnamed_single_index",
+            pytest.raises(ValueError),
+        ),
+        (
+            "gdf_regions",
+            "gdf_features",
+            "gdf_three_level_multi_index",
+            pytest.raises(ValueError),
+        ),
+        (
+            "gdf_incorrectly_named_single_index",
+            "gdf_features",
+            "gdf_joint",
+            pytest.raises(ValueError),
+        ),
+        (
+            "gdf_regions",
+            "gdf_incorrectly_named_single_index",
+            "gdf_joint",
+            pytest.raises(ValueError),
+        ),
+    ],
+)
+def test_incorrect_indexes(
+    regions_fixture: str, features_fixture: str, joint_fixture: str, expectation: Any, request: Any
+) -> None:
+    """Test if cannot embed with incorrect dataframe indexes."""
+    regions_gdf = request.getfixturevalue(regions_fixture)
+    features_gdf = request.getfixturevalue(features_fixture)
+    joint_gdf = request.getfixturevalue(joint_fixture)
+
+    with expectation:
+        CountEmbedder().embed(
+            regions_gdf=regions_gdf, features_gdf=features_gdf, joint_gdf=joint_gdf
+        )
