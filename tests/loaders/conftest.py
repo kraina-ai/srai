4b59f0d8582fd10e43edb1780f363cae3cb40391
Piotr Gramacki <p.gramacki@gmail.com>
2023-01-20 20:44:49 +0100

    test: add GTFSLoader tests

--- tests/loaders/conftest.py
+++ tests/loaders/conftest.py
@@ -1,7 +1,11 @@
 """Conftest for loaders."""
 
+from datetime import datetime
+from typing import Any
+
 import pandas as pd
 import pytest
+from pytest_mock import MockerFixture
 
 
 @pytest.fixture  # type: ignore
@@ -28,3 +32,81 @@ def gtfs_validation_error() -> pd.DataFrame:
             "rows": [[1, 2], [3, 4], [5, 6], [7, 8]],
         }
     )
+
+
+@pytest.fixture  # type: ignore
+def stop_time_series() -> pd.DataFrame:
+    """Get mocked stop time series."""
+    ts = pd.DataFrame.from_dict(
+        {
+            "datetime": pd.DatetimeIndex(
+                [datetime(2022, 1, 2, 12, 0), datetime(2022, 1, 2, 13, 0)]
+            ),
+            "42": pd.Series([0, 2]),
+            "76": pd.Series([12, 12]),
+        }
+    ).set_index("datetime")
+
+    ts.columns = pd.MultiIndex.from_tuples(
+        [("num_trips", "42"), ("num_trips", "76")], names=["indicator", "stop_id"]
+    )
+
+    return ts
+
+
+@pytest.fixture  # type: ignore
+def stop_times() -> pd.DataFrame:
+    """Get mocked stop times."""
+    return pd.DataFrame(
+        {
+            "trip_id": ["1", "1", "2", "2"],
+            "arrival_time": ["12:00:00", "13:00:00", "12:00:00", "13:00:00"],
+            "departure_time": ["12:00:00", "12:00:00", "13:00:00", "13:00:00"],
+            "stop_id": ["42", "76", "42", "76"],
+        }
+    )
+
+
+@pytest.fixture  # type: ignore
+def trips() -> pd.DataFrame:
+    """Get mocked trips."""
+    return pd.DataFrame(
+        {
+            "trip_id": ["1", "2"],
+            "trip_headsign": ["A", "B"],
+        }
+    )
+
+
+@pytest.fixture  # type: ignore
+def stops() -> pd.DataFrame:
+    """Get mocked stops."""
+    return pd.DataFrame(
+        {
+            "stop_id": ["42", "76"],
+            "stop_lat": [51.198083, 51.107133],
+            "stop_lon": [16.905892, 17.019394],
+        }
+    )
+
+
+@pytest.fixture  # type: ignore
+def feed(
+    mocker: MockerFixture,
+    stop_time_series: pd.DataFrame,
+    stop_times: pd.DataFrame,
+    trips: pd.DataFrame,
+    stops: pd.DataFrame,
+) -> Any:
+    """Get mocked feed."""
+    feed_mock = mocker.MagicMock()
+    feed_mock.configure_mock(
+        **{
+            "get_first_week.return_value": ["", "", "20220102"],
+            "compute_stop_time_series.return_value": stop_time_series,
+            "stop_times": stop_times,
+            "trips": trips,
+            "stops": stops,
+        }
+    )
+    return feed_mock
--- tests/loaders/test_gtfs_loader.py
+++ tests/loaders/test_gtfs_loader.py
@@ -1,9 +1,15 @@
 """GTFS Loader tests."""
+from pathlib import Path
+from typing import Any
+from unittest import TestCase
+
 import pandas as pd
 import pytest
 from pytest_mock import MockerFixture
 
 from srai.loaders import GTFSLoader
+
+ut = TestCase()
 
 
 def test_validation_ok(mocker: MockerFixture, gtfs_validation_ok: pd.DataFrame) -> None:
@@ -38,3 +44,51 @@ def test_validation_warning(mocker: MockerFixture, gtfs_validation_error: pd.Dat
     loader = GTFSLoader()
     loader._validate_feed(feed_mock, fail=False)
     warning_mock.assert_called_once()
+
+
+def test_gtfs_loader(feed: Any, mocker: MockerFixture, gtfs_validation_ok: pd.DataFrame) -> None:
+    """Test GTFSLoader."""
+    feed.validate.return_value = gtfs_validation_ok
+    mocker.patch("gtfs_kit.read_feed", return_value=feed)
+
+    loader = GTFSLoader()
+    features = loader.load(Path("feed.zip").resolve())
+
+    ut.assertCountEqual(features.index, ["42", "76"])
+    ut.assertCountEqual(
+        features.columns,
+        [
+            "trip_count_at_12",
+            "trip_count_at_13",
+            "directions_at_12",
+            "directions_at_13",
+            "geometry",
+        ],
+    )
+
+
+def test_gtfs_loader_with_invalid_feed(
+    feed: Any, mocker: MockerFixture, gtfs_validation_error: pd.DataFrame
+) -> None:
+    """Test GTFSLoader with invalid feed."""
+    feed.validate.return_value = gtfs_validation_error
+    mocker.patch("gtfs_kit.read_feed", return_value=feed)
+    warning_mock = mocker.patch("warnings.warn")
+
+    loader = GTFSLoader()
+    with pytest.raises(ValueError):
+        loader.load(Path("feed.zip").resolve())
+        warning_mock.assert_called_once()
+
+
+def test_gtfs_loader_skip_validation(
+    feed: Any, mocker: MockerFixture, gtfs_validation_ok: pd.DataFrame
+) -> None:
+    """Test GTFSLoader with invalid feed."""
+    feed.validate.return_value = gtfs_validation_ok
+    mocker.patch("gtfs_kit.read_feed", return_value=feed)
+
+    loader = GTFSLoader()
+    loader.load(Path("feed.zip").resolve(), skip_validation=True)
+
+    feed.validate.assert_not_called()
