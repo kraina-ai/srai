0b5f0af22bb43195308cdc3f51659e0d7e1b9a79
Szymon Woźniak <swozniak6@gmail.com>
2023-02-24 23:00:38 +0100

    feat: add generic functions for getting neighbours up to, or at a certain distance, add more efficient implementation for H3Neighbourhood

--- srai/neighbourhood/h3.py
+++ srai/neighbourhood/h3.py
@@ -3,14 +3,14 @@ H3 neighbourhood.
 
 This module contains the H3Neighbourhood class, that allows to get the neighbours of an H3 region.
 """
-from typing import List
+from typing import List, Set
 
 import h3
 
 from .neighbourhood import Neighbourhood
 
 
-class H3Neighbourhood(Neighbourhood):
+class H3Neighbourhood(Neighbourhood[str]):
     """
     H3 Neighbourhood.
 
@@ -19,19 +19,47 @@ class H3Neighbourhood(Neighbourhood):
 
     def get_neighbours(self, index: str, exclude_anchor: bool = True) -> List[str]:
         """
-        Get the neighbours of an H3 region using its index.
+        Get the direct neighbours of an H3 region using its index.
 
         Args:
-            index (str): Unique identifier of the region.
-                Depends on the implementation.
+            index (str): H3 index of the region.
+            exclude_anchor (bool): Whether to exclude the anchor index from the neighbours.
+                Defaults to True.
+
+        Returns:
+            List[str]: Indexes of the neighbours.
+        """
+        return self.get_neighbours_up_to_distance(index, 1, exclude_anchor)
+
+    def get_neighbours_up_to_distance(
+        self, index: str, distance: int, exclude_anchor: bool = True
+    ) -> List[str]:
+        """
+        Get the neighbours of an H3 region up to a certain distance.
+
+        Args:
+            index (str): H3 index of the region.
+            distance (int): Distance to the neighbours.
             exclude_anchor (bool): Whether to exclude the anchor index from the neighbours.
 
         Returns:
             List[str]: Indexes of the neighbours.
         """
-        if exclude_anchor:
-            neighbour_function = h3.grid_ring
-        else:
-            neighbour_function = h3.grid_disk
-        neighbours: List[str] = neighbour_function(index, 1)
+        neighbours: Set[str] = h3.grid_disk(index, distance)
+        if exclude_anchor and index in neighbours:
+            neighbours.remove(index)
+        return list(neighbours)
+
+    def get_neighbours_at_distance(self, index: str, distance: int) -> List[str]:
+        """
+        Get the neighbours of an H3 region at a certain distance.
+
+        Args:
+            index (str): _description_
+            distance (int): _description_
+
+        Returns:
+            List[str]: _description_
+        """
+        neighbours: List[str] = h3.grid_ring(index, distance)
         return neighbours
--- srai/neighbourhood/neighbourhood.py
+++ srai/neighbourhood/neighbourhood.py
@@ -1,22 +1,95 @@
 """Neighbourhood interface."""
 
 from abc import ABC, abstractmethod
-from typing import Any, List
+from queue import Queue
+from typing import Generic, List, Tuple, TypeVar
+
+from functional import seq
+
+IndexType = TypeVar("IndexType")
 
 
-class Neighbourhood(ABC):
+class Neighbourhood(ABC, Generic[IndexType]):
     """Neighbourhood interface."""
 
     @abstractmethod
-    def get_neighbours(self, index: Any) -> List[Any]:
+    def get_neighbours(self, index: IndexType, exclude_anchor: bool = True) -> List[IndexType]:
         """
         Get the direct neighbours of a region using its index.
 
         Args:
             index (Any): Unique identifier of the region.
                 Dependant on the implementation.
+            exclude_anchor (bool): Whether to exclude the anchor index from the neighbours.
 
         Returns:
             List[Any]: Indexes of the neighbours.
         """
         pass
+
+    def get_neighbours_up_to_distance(
+        self, index: IndexType, distance: int, exclude_anchor: bool = True
+    ) -> List[IndexType]:
+        """
+        Get the neighbours of a region up to a certain distance.
+
+        Args:
+            index (Any): Unique identifier of the region.
+                Dependant on the implementation.
+            distance (int): Maximum distance to the neighbours.
+            exclude_anchor (bool): Whether to exclude the anchor index from the neighbours.
+
+        Returns:
+            List[Any]: Indexes of the neighbours.
+        """
+        neighbours_with_distances = self._get_neighbours_with_distances(index, distance)
+        unique_neighbours = seq(neighbours_with_distances).map(lambda x: x[0]).to_set()
+        if exclude_anchor and index in unique_neighbours:
+            unique_neighbours.remove(index)
+        return list(unique_neighbours)
+
+    def get_neighbours_at_distance(self, index: IndexType, distance: int) -> List[IndexType]:
+        """
+        Get the neighbours of a region at a certain distance.
+
+        Args:
+            index (Any): Unique identifier of the region.
+                Dependant on the implementation.
+            distance (int): Distance to the neighbours.
+
+        Returns:
+            List[Any]: Indexes of the neighbours.
+        """
+        neighbours_up_to_distance = self._get_neighbours_with_distances(index, distance)
+        neighbours_at_distance: List[IndexType] = (
+            seq(neighbours_up_to_distance)
+            .filter(lambda x: x[1] == distance)
+            .map(lambda x: x[0])
+            .to_list()
+        )
+        return neighbours_at_distance
+
+    def _get_neighbours_with_distances(
+        self, index: IndexType, distance: int
+    ) -> List[Tuple[IndexType, int]]:
+        visited = set()
+        visited_with_distances = []
+        to_visit: Queue[Tuple[IndexType, int]] = Queue()
+
+        visited.add(index)
+        visited_with_distances.append((index, 0))
+        to_visit.put((index, 0))
+
+        while not to_visit.empty():
+            current, current_distance = to_visit.get()
+
+            visited.add(current)
+            if current_distance < distance:
+                current_neighbours = self.get_neighbours(current)
+                for neighbour in current_neighbours:
+                    if neighbour not in visited:
+                        to_visit.put((neighbour, current_distance + 1))
+                        visited_with_distances.append((neighbour, current_distance + 1))
+                        visited.add(neighbour)
+
+        return visited_with_distances
