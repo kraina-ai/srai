709ef172d30352836e3b3e5921b8774ae2ef7619
Kamil Raczycki <raczyckikamil@gmail.com>
2023-03-13 08:37:53 +0100

    feat: modify plotly plotting function

--- srai/plotting/plotly_wrapper.py
+++ srai/plotting/plotly_wrapper.py
@@ -1,214 +1,168 @@
 # noqa
 """TODO."""
-from typing import Any
+from typing import List, Optional
 
 import geopandas as gpd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objs as go
+from shapely.geometry import Point
+
+from srai.utils.constants import REGIONS_INDEX, WGS84_CRS
 
 
-def plot_regions_gdf(regions_gdf: gpd.GeoDataFrame) -> go.Figure:  # noqa
-    """TODO."""
-    center_point = regions_gdf.dissolve().centroid[0]
-    minx, miny, maxx, maxy = regions_gdf.geometry.total_bounds
-    # zoom = _get_zoom_level_for_bounds(lat_min=miny, lat_max=maxy, lon_min=minx, lon_max=maxx)
-    # zoom = _calculate_mapbox_zoom(lat_min=miny, lat_max=maxy, lon_min=minx, lon_max=maxx)
-    zoom = get_plotting_zoom_level(lat_min=miny, lat_max=maxy, lon_min=minx, lon_max=maxx)
-    # zoom, center = zoom_center(maxlat=maxy, maxlon=maxx, minlat=miny, minlon=minx)
-    print(minx, miny, maxx, maxy, zoom)
-    # print(center_point, center)
+def plot_regions_gdf(
+    regions_gdf: gpd.GeoDataFrame,
+    return_plot: bool = False,
+    mapbox_style: str = "open-street-map",
+    mapbox_accesstoken: Optional[str] = None,
+    renderer: Optional[str] = "notebook_connected",
+    zoom: Optional[float] = None,
+    height: Optional[float] = None,
+    width: Optional[float] = None,
+) -> Optional[go.Figure]:
+    """
+    Plot regions shapes using Plotly library.
+
+    For more info about parameters, check https://plotly.com/python/mapbox-layers/.
+
+    Args:
+        regions_gdf (gpd.GeoDataFrame): Region indexes and geometries to plot.
+        return_plot (bool, optional): Flag whether to return the Figure object or not.
+            If `True`, the plot won't be displayed automatically. Defaults to False.
+        mapbox_style (str, optional): Map style background. Defaults to "open-street-map".
+        mapbox_accesstoken (str, optional): Access token required for mapbox based map backgrounds.
+            Defaults to None.
+        renderer (str, optional): Name of renderer used for displaying the figure.
+            For all descriptions, look here: https://plotly.com/python/renderers/.
+            Defaults to "notebook_connected".
+        zoom (float, optional): Map zoom. If not filled, will be approximated based on
+            the bounding box of regions. Defaults to None.
+        height (float, optional): Height of the plot. Defaults to None.
+        width (float, optional): Width of the plot. Defaults to None.
+
+    Returns:
+        Optional[go.Figure]: Figure of the plot. Will be returned if `return_plot` is set to `True`.
+    """
+    regions_gdf_copy = regions_gdf.copy()
+    regions_gdf_copy[REGIONS_INDEX] = regions_gdf_copy.index
+    return _plot_regions_gdf(
+        regions_gdf=regions_gdf_copy,
+        hover_column_name=REGIONS_INDEX,
+        color_feature_column=REGIONS_INDEX,
+        hover_data=[],
+        return_plot=return_plot,
+        mapbox_style=mapbox_style,
+        mapbox_accesstoken=mapbox_accesstoken,
+        renderer=renderer,
+        zoom=zoom,
+        height=height,
+        width=width,
+    )
+
+
+def _plot_regions_gdf(
+    regions_gdf: gpd.GeoDataFrame,
+    hover_column_name: str,
+    color_feature_column: str,
+    hover_data: List[str],
+    return_plot: bool = False,
+    mapbox_style: str = "open-street-map",
+    mapbox_accesstoken: Optional[str] = None,
+    renderer: Optional[str] = "notebook_connected",
+    zoom: Optional[float] = None,
+    height: Optional[float] = None,
+    width: Optional[float] = None,
+) -> Optional[go.Figure]:
+    """
+    Plot regions shapes using Plotly library.
+
+    For more info about parameters, check https://plotly.com/python/mapbox-layers/.
+
+    Args:
+        regions_gdf (gpd.GeoDataFrame): Region indexes and geometries to plot.
+        hover_column_name (str): Column name used for hover popup title.
+        color_feature_column (str): Column name used for colouring the plot.
+        hover_data (List[str]): List of column names displayed additionally on hover.
+        return_plot (bool, optional): Flag whether to return the Figure object or not.
+            If `True`, the plot won't be displayed automatically. Defaults to False.
+        mapbox_style (str, optional): Map style background. Defaults to "open-street-map".
+        mapbox_accesstoken (str, optional): Access token required for mapbox based map backgrounds.
+            Defaults to None.
+        renderer (str, optional): Name of renderer used for displaying the figure.
+            For all descriptions, look here: https://plotly.com/python/renderers/.
+            Defaults to "notebook_connected".
+        zoom (float, optional): Map zoom. If not filled, will be approximated based on
+            the bounding box of regions. Defaults to None.
+        height (float, optional): Height of the plot. Defaults to None.
+        width (float, optional): Width of the plot. Defaults to None.
+
+    Returns:
+        Optional[go.Figure]: Figure of the plot. Will be returned if `return_plot` is set to `True`.
+    """
+    center_point = _calculate_map_centroid(regions_gdf)
+    if not zoom:
+        zoom = _calculate_mapbox_zoom(regions_gdf)
+
     fig = px.choropleth_mapbox(
         regions_gdf,
         geojson=regions_gdf,
-        color=regions_gdf.index,
-        locations=regions_gdf.index,
+        color=color_feature_column,
+        hover_name=hover_column_name,
+        hover_data=hover_data,
+        locations=REGIONS_INDEX,
         center={"lon": center_point.x, "lat": center_point.y},
-        mapbox_style="carto-positron",
         zoom=zoom,
     )
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
     fig.update_traces(marker={"opacity": 0.6}, selector=dict(type="choroplethmapbox"))
     fig.update_traces(showlegend=False)
-    fig.update_layout(autosize=True, margin={"r": 0, "t": 0, "l": 0, "b": 0})
-    # width = fig.layout.width
-    # height = fig.layout.height
+    fig.update_coloraxes(showscale=False)
+    fig.update_layout(height=height, width=width, margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    fig.update_layout(mapbox_style=mapbox_style, mapbox_accesstoken=mapbox_accesstoken)
 
-    print(fig.layout)
-    # minx, miny, maxx, maxy = regions_gdf.geometry.total_bounds
-    # fig.update_geos(
-    #     projection_type="equirectangular",
-    #     # lataxis_range=[miny, maxy],
-    #     # lonaxis_range=[minx, maxx],
-    #     fitbounds="locations",
-    #     showlakes=False,
-    #     showcountries=False,
-    #     showframe=False,
-    #     resolution=50,
-    # )
-    # fig.update_layout(height=600, width=800, margin={"r": 0, "t": 0, "l": 0, "b": 0})
-    print("XD")
-
-    # fig.show(renderer="png")  # replace with fig.show() to allow interactivity
-    fig.show()
-
-
-def _calculate_mapbox_zoom(
-    lat_min: float, lat_max: float, lon_min: float, lon_max: float
-) -> Any:  # noqa
-    """TODO."""
-    max_bound = max(abs(lat_max - lat_min), abs(lon_max - lon_min)) * 111
-    zoom = 13 - np.log(max_bound)
-    return zoom
-
-
-def _get_zoom_level_for_bounds(
-    lat_min: float, lat_max: float, lon_min: float, lon_max: float
-) -> Any:  # noqa
-    """TODO."""
-    dx = abs(lat_max - lat_min)
-    dy = abs(lon_max - lon_min)
-    d = dy if dy > dx else dx
-
-    zoom_delta_lookup = {
-        100: 0,
-        75: 0.2,
-        50: 0.4,
-        40: 1.2,
-        30: 1.4,
-        20: 1.6,
-        15: 1.7,
-        10: 2.1,
-        5: 2.7,
-        2.5: 3.2,
-        1: 3.8,
-        0.5: 6,
-        0.25: 7.2,
-        0.125: 7.6,
-        0.1: 8.2,
-        0.01: 8.8,
-        0.001: 9,
-        0: 9.6,
-    }
-
-    for delta, zoom_value in zoom_delta_lookup.items():
-        print(d, delta, zoom_value)
-        if d > delta:
-            return zoom_value
-
-
-def zoom_center(
-    maxlon: float,
-    minlon: float,
-    maxlat: float,
-    minlat: float,
-    projection: str = "mercator",
-    # width_to_height: float = 2.0,
-) -> Any:  # noqa
-    """
-    Finds optimal zoom and centering for a plotly mapbox. Must be passed (lons & lats) or lonlats.
-    Temporary solution awaiting official implementation, see:
-    https://github.com/plotly/plotly.js/issues/3434.
-
-    Parameters
-    --------
-    lons: tuple, optional, longitude component of each location
-    lats: tuple, optional, latitude component of each location
-    lonlats: tuple, optional, gps locations
-    format: str, specifying the order of longitud and latitude dimensions,
-        expected values: 'lonlat' or 'latlon', only used if passed lonlats
-    projection: str, only accepting 'mercator' at the moment,
-        raises `NotImplementedError` if other is passed
-    width_to_height: float, expected ratio of final graph's with to height,
-        used to select the constrained axis.
-
-    Returns
-    --------
-    zoom: float, from 1 to 20
-    center: dict, gps position with 'lon' and 'lat' keys
-
-    >>> print(zoom_center((-109.031387, -103.385460),
-    ...     (25.587101, 31.784620)))
-    (5.75, {'lon': -106.208423, 'lat': 28.685861})
-    """
-    center = {"lon": round((maxlon + minlon) / 2, 6), "lat": round((maxlat + minlat) / 2, 6)}
-
-    # longitudinal range by zoom level (20 to 1)
-    # in degrees, if centered at equator
-    lon_zoom_range = np.array(
-        [
-            0.0007,
-            0.0014,
-            0.003,
-            0.006,
-            0.012,
-            0.024,
-            0.048,
-            0.096,
-            0.192,
-            0.3712,
-            0.768,
-            1.536,
-            3.072,
-            6.144,
-            11.8784,
-            23.7568,
-            47.5136,
-            98.304,
-            190.0544,
-            360.0,
-        ]
-    )
-
-    if projection == "mercator":
-        margin = 1.2
-        # height = (maxlat - minlat) * margin * width_to_height
-        height = (maxlat - minlat) * margin
-        width = (maxlon - minlon) * margin
-        lon_zoom = np.interp(width, lon_zoom_range, range(20, 0, -1))
-        lat_zoom = np.interp(height, lon_zoom_range, range(20, 0, -1))
-        zoom = round(min(lon_zoom, lat_zoom), 2)
+    if return_plot:
+        return fig
     else:
-        raise NotImplementedError(f"{projection} projection is not implemented")
+        fig.show(renderer=renderer)
+        return None
 
-    return zoom, center
 
-
-def get_plotting_zoom_level(
-    lat_min: float, lat_max: float, lon_min: float, lon_max: float
-) -> Any:  # noqa
+def _calculate_map_centroid(regions_gdf: gpd.GeoDataFrame) -> Point:
     """
-    Function documentation: Basic framework adopted from Krichardson under the following thread:
-    https://community.plotly.com/t/dynamic-zoom-for-mapbox/32658/6.
+    Calculate regions centroid using Equal Area Cylindrical projection [1].
 
-    # NOTE:
-    # THIS IS A TEMPORARY SOLUTION UNTIL THE DASH TEAM IMPLEMENTS DYNAMIC ZOOM
-    # in their plotly-functions associated with mapbox, such as go.Densitymapbox() etc.
+    Args:
+        regions_gdf (gpd.GeoDataFrame): Region indexes and geometries.
 
-    Returns the appropriate zoom-level for these plotly-mapbox-graphics along with
-    the center coordinate tuple of all provided coordinate tuples.
+    Returns:
+        Point: Center point in WGS84 units.
+
+    References:
+        1. https://proj.org/operations/projections/cea.html
+    """
+    center_point = regions_gdf.to_crs("+proj=cea").dissolve().centroid.to_crs(WGS84_CRS)[0]
+    return center_point
+
+
+# Inspired by:
+# https://stackoverflow.com/a/65043576/7766101
+def _calculate_mapbox_zoom(
+    regions_gdf: gpd.GeoDataFrame,
+) -> float:
+    """
+    Calculate approximate zoom for a plotly figure.
+
+    Currently Plotly doesn't implement auto-fit feature for mapbox plots.
+
+    Args:
+        regions_gdf (gpd.GeoDataFrame): Region indexes and geometries.
+
+    Returns:
+        float: zoom level for a mapbox plot.
     """
 
-    width = abs(lat_max - lat_min)
-    height = abs(lon_max - lon_min)
-
-    # Otherwise, get the area of the bounding box in order to calculate a zoom-level
-    area = height * width
-
-    print(width, height, area)
-
-    # * 1D-linear interpolation with numpy:
-    # - Pass the area as the only x-value and not as a list, in order to return a scalar as well
-    # - The x-points "xp" should be in parts in comparable order of magnitude of the given area
-    # - The zoom-levels are adapted to the areas, i.e. start with the smallest area possible of 0
-    # which leads to the highest possible zoom value 20, and so forth decreasing with increasing
-    # areas as these variables are antiproportional
-    zoom = np.interp(
-        x=area,
-        xp=[0, 5**-10, 4**-10, 3**-10, 2**-10, 1**-10, 1**-5],
-        # fp=[20, 17, 16, 15, 14, 7, 5],
-        fp=[20, 15, 14, 13, 12, 7, 5],
-    )
-
-    # Finally, return the zoom level and the associated boundary-box center coordinates
+    minx, miny, maxx, maxy = regions_gdf.geometry.total_bounds
+    max_bound = max(abs(maxx - minx), abs(maxy - miny)) * 111
+    zoom = float(12.5 - np.log(max_bound))
     return zoom
