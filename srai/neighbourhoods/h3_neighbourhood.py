d3a4de5e930f185c2961762b899e737bbd25bd41
Szymon Woźniak <szymon.wozniak@brand24.com>
2023-03-13 21:34:51 +0100

    chore: add example notebook for H3Neighbourhood, add folium and mapclassify dependencies, add notice to H3Neighbourhood

--- srai/neighbourhoods/h3_neighbourhood.py
+++ srai/neighbourhoods/h3_neighbourhood.py
@@ -24,6 +24,10 @@ class H3Neighbourhood(Neighbourhood[str]):
 
         If a regions GeoDataFrame is provided, only the neighbours
         that are in the regions GeoDataFrame will be returned by the methods of this instance.
+        NOTICE: If a region is a part of the k-th ring of a region
+            and is included in the GeoDataFrame, it will be returned
+            by get_neighbours_at_distance method with distance k
+            even when there is no path of length k between the two regions.
 
         Args:
             regions_gdf (Optional[gpd.GeoDataFrame], optional): The regions that are being analyzed.
