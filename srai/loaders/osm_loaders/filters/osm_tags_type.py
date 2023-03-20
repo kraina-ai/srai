ab558fd53cca26056f0d2d9cf9fc9aff66a5dac2
Kamil Raczycki <raczyckikamil@gmail.com>
2023-03-20 20:20:01 +0100

    chore: update srai/loaders/osm_loaders/filters/osm_tags_type.py
    Co-authored-by: Szymon Woźniak <36135334+simonusher@users.noreply.github.com>

--- srai/loaders/osm_loaders/filters/osm_tags_type.py
+++ srai/loaders/osm_loaders/filters/osm_tags_type.py
@@ -1,4 +1,4 @@
-"""Module contains dedicated typing wrapper for OSM tags filter."""
+"""Module contains a dedicated type alias for OSM tags filter."""
 from typing import Dict, List, Union
 
 osm_tags_type = Dict[str, Union[List[str], str, bool]]
