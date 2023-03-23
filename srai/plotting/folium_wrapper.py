e5a4e563f30a491df02e2bbd5e668e920cfbf901
Kamil Raczycki <raczyckikamil@gmail.com>
2023-03-23 18:51:46 +0100

    feat: added new plotting function

--- srai/plotting/folium_wrapper.py
+++ srai/plotting/folium_wrapper.py
@@ -7,9 +7,11 @@ function.
 from itertools import cycle, islice
 from typing import List, Optional, Set, Union
 
+import branca.colormap as cm
 import folium
 import geopandas as gpd
 import numpy as np
+import pandas as pd
 import plotly.express as px
 
 from srai.constants import REGIONS_INDEX
@@ -22,7 +24,7 @@ def plot_regions(
     tiles_style: str = "OpenStreetMap",
     height: Union[str, float] = "100%",
     width: Union[str, float] = "100%",
-    colormap: List[str] = px.colors.qualitative.Bold,
+    colormap: Union[str, List[str]] = px.colors.qualitative.Bold,
     map: Optional[folium.Map] = None,
 ) -> folium.Map:
     """
@@ -33,8 +35,8 @@ def plot_regions(
         tiles_style (str, optional): Map style background. Defaults to "OpenStreetMap".
         height (Union[str, float], optional): Height of the plot. Defaults to "100%".
         width (Union[str, float], optional): Width of the plot. Defaults to "100%".
-        colormap (List[str], optional): Colormap to apply to the regions.
-            Defaults to `px.colors.sequential.Bold` from plotly library.
+        colormap (Union[str, List[str]], optional): Colormap to apply to the regions.
+            Defaults to `px.colors.qualitative.Bold` from plotly library.
         map (folium.Map, optional): Existing map instance on which to draw the plot.
             Defaults to None.
 
@@ -52,6 +54,59 @@ def plot_regions(
         cmap=colormap,
         categorical=True,
         style_kwds=dict(color="#444", opacity=0.5, fillOpacity=0.5),
+        m=map,
+    )
+
+
+def plot_numeric_data(
+    regions_gdf: gpd.GeoDataFrame,
+    embedding_df: Union[pd.DataFrame, gpd.GeoDataFrame],
+    data_column: str,
+    tiles_style: str = "OpenStreetMap",
+    height: Union[str, float] = "100%",
+    width: Union[str, float] = "100%",
+    colormap: Union[str, List[str]] = px.colors.sequential.Sunsetdark,
+    map: Optional[folium.Map] = None,
+) -> folium.Map:
+    """
+    Plot numerical data within regions shapes using Folium library.
+
+    Args:
+        regions_gdf (gpd.GeoDataFrame): Region indexes and geometries to plot.
+        embedding_df (Union[pd.DataFrame, gpd.GeoDataFrame]): Region indexes and numerical data
+            to plot.
+        data_column (str): Name of the column used to colour the regions.
+        tiles_style (str, optional): Map style background. Defaults to "OpenStreetMap".
+        height (Union[str, float], optional): Height of the plot. Defaults to "100%".
+        width (Union[str, float], optional): Width of the plot. Defaults to "100%".
+        colormap (Union[str, List[str]], optional): Colormap to apply to the regions.
+            Defaults to px.colors.sequential.Sunsetdark.
+        map (folium.Map, optional): Existing map instance on which to draw the plot.
+            Defaults to None.
+
+    Returns:
+        folium.Map: Generated map.
+    """
+    regions_gdf_copy = regions_gdf.copy()
+    regions_gdf_copy = regions_gdf_copy.merge(embedding_df, on=REGIONS_INDEX)
+
+    if not isinstance(colormap, str):
+        colormap = _generate_linear_colormap(
+            colormap,
+            min_value=regions_gdf_copy[data_column].min(),
+            max_value=regions_gdf_copy[data_column].max(),
+        )
+
+    return regions_gdf_copy.reset_index().explore(
+        column=data_column,
+        tooltip=[REGIONS_INDEX, data_column],
+        tiles=tiles_style,
+        height=height,
+        width=width,
+        legend=True,
+        cmap=colormap,
+        categorical=False,
+        style_kwds=dict(color="#444", opacity=0.5, fillOpacity=0.8),
         m=map,
     )
 
@@ -113,7 +168,7 @@ def plot_all_neighbourhood(
     tiles_style: str = "OpenStreetMap",
     height: Union[str, float] = "100%",
     width: Union[str, float] = "100%",
-    colormap: List[str] = px.colors.sequential.Agsunset_r,
+    colormap: Union[str, List[str]] = px.colors.sequential.Agsunset_r,
     map: Optional[folium.Map] = None,
 ) -> folium.Map:
     """
@@ -127,7 +182,7 @@ def plot_all_neighbourhood(
         tiles_style (str, optional): Map style background. Defaults to "OpenStreetMap".
         height (Union[str, float], optional): Height of the plot. Defaults to "100%".
         width (Union[str, float], optional): Width of the plot. Defaults to "100%".
-        colormap (List[str], optional): Colormap to apply to the neighbourhoods.
+        colormap (Union[str, List[str]], optional): Colormap to apply to the neighbourhoods.
             Defaults to `px.colors.sequential.Agsunset_r` from plotly library.
         map (folium.Map, optional): Existing map instance on which to draw the plot.
             Defaults to None.
@@ -153,9 +208,10 @@ def plot_all_neighbourhood(
             regions_gdf.index
         )
 
-    colorscale = _generate_colorscale(
-        distance, colorscale=_resample_plotly_colorscale(colormap, min(distance, 10))
-    )
+    if not isinstance(colormap, str):
+        colormap = _generate_colormap(
+            distance, colormap=_resample_plotly_colormap(colormap, min(distance, 10))
+        )
 
     return regions_gdf_copy.reset_index().explore(
         column="region",
@@ -163,7 +219,7 @@ def plot_all_neighbourhood(
         tiles=tiles_style,
         height=height,
         width=width,
-        cmap=colorscale,
+        cmap=colormap,
         categorical=True,
         categories=["selected", *list(range(distance))[1:], "other"],
         style_kwds=dict(color="#444", opacity=0.5, fillOpacity=0.8),
@@ -172,17 +228,24 @@ def plot_all_neighbourhood(
     )
 
 
-def _resample_plotly_colorscale(colorscale: List[str], steps: int) -> List[str]:
-    resampled_colorscale: List[str] = px.colors.sample_colorscale(
-        colorscale, np.linspace(0, 1, num=steps)
+def _resample_plotly_colormap(colormap: List[str], steps: int) -> List[str]:
+    resampled_colormap: List[str] = px.colors.sample_colorscale(
+        colormap, np.linspace(0, 1, num=steps)
     )
-    return resampled_colorscale
+    return resampled_colormap
 
 
-def _generate_colorscale(
+def _generate_colormap(
     distance: int,
-    colorscale: List[str],
+    colormap: List[str],
     selected_color: str = "rgb(242, 242, 242)",
     other_color: str = "rgb(153, 153, 153)",
 ) -> List[str]:
-    return [selected_color, *islice(cycle(colorscale), None, distance - 1), other_color]
+    return [selected_color, *islice(cycle(colormap), None, distance - 1), other_color]
+
+
+def _generate_linear_colormap(
+    colormap: List[str], min_value: float, max_value: float
+) -> cm.LinearColormap:
+    values, _ = px.colors.convert_colors_to_same_type(colormap, colortype="tuple")
+    return cm.LinearColormap(values, vmin=min_value, vmax=max_value)
