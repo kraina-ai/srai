d52c9c50ead987e9897f1bc991e8ecc7d1d2d641
Calychas <kacper.lesniara@gmail.com>
2023-03-26 16:53:31 +0200

    fix: missing import

--- tests/loaders/osm_loaders/test_osm_online_loader.py
+++ tests/loaders/osm_loaders/test_osm_online_loader.py
@@ -22,7 +22,7 @@ def mock_osmnx(
     polygon_1, polygon_2 = two_polygons_area_gdf["geometry"]
     empty_polygon = area_with_no_objects_gdf["geometry"][0]
 
-    def mock_geometries_from_polygon(polygon: Polygon, tags: osm_tags_type) -> gpd.GeoDataFrame:
+    def mock_geometries_from_polygon(polygon: "Polygon", tags: osm_tags_type) -> gpd.GeoDataFrame:
         tag_key, tag_value = list(tags.items())[0]
         gdf = gdfs[tag_key]
         if tag_value is True:
