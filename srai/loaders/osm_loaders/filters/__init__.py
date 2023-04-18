8b9de5d5177e22864dcd4d7d1d4f0b8effc55ede
Kamil Raczycki <raczyckikamil@gmail.com>
2023-04-18 19:37:37 +0200

    chore: add merging test skeleton

--- srai/loaders/osm_loaders/filters/__init__.py
+++ srai/loaders/osm_loaders/filters/__init__.py
@@ -1,6 +1,7 @@
 """Filters."""
 
+from .geofabrik import GEOFABRIK_LAYERS
 from .hex2vec import HEX2VEC_FILTER
 from .popular import get_popular_tags
 
-__all__ = ["HEX2VEC_FILTER", "get_popular_tags"]
+__all__ = ["GEOFABRIK_LAYERS", "HEX2VEC_FILTER", "get_popular_tags"]
---
+++ tests/loaders/osm_loaders/filters/test_merge_filter_types.py
@@ -0,0 +1,30 @@
+"""Tests for merging OSM Loaders filters."""
+from unittest import TestCase
+
+import pytest
+
+from srai.loaders.osm_loaders.filters.osm_tags_type import (
+    grouped_osm_tags_type,
+    merge_grouped_osm_tags_type,
+    osm_tags_type,
+)
+
+ut = TestCase()
+
+
+@pytest.fixture  # type: ignore
+def expected_result_min_count_8m() -> osm_tags_type:
+    """Get expected results when using `min_count=8_000_000`."""
+    return {
+        "natural": ["wood"],
+        "landuse": ["farmland", "residential"],
+    }
+
+
+def test_merge_grouped_filters() -> None:
+    """Test merging grouped tags filter into a base osm filter."""
+    base_filter: osm_tags_type = {}
+    grouped_filter: grouped_osm_tags_type = {}
+    merged_filters = merge_grouped_osm_tags_type(grouped_filter)
+
+    ut.assertDictEqual(base_filter, merged_filters)
