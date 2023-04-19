554d9c80bf390c6569e72f21e4b5cd1ae3dfabc9
Piotr <p.gramacki@gmail.com>
2023-04-19 18:39:00 +0200

    feat: unify folder structure of gtfs2vec with rest of the library

--- srai/embedders/__init__.py
+++ srai/embedders/__init__.py
@@ -2,7 +2,7 @@
 
 from ._base import Embedder
 from .count_embedder import CountEmbedder
-from .gtfs2vec_embedder import GTFS2VecEmbedder
+from .gtfs2vec import GTFS2VecEmbedder
 from .highway2vec import Highway2VecEmbedder
 
 __all__ = ["Embedder", "CountEmbedder", "GTFS2VecEmbedder", "Highway2VecEmbedder"]
---
+++ srai/embedders/gtfs2vec/__init__.py
@@ -0,0 +1,6 @@
+"""Models."""
+
+from .embedder import GTFS2VecEmbedder
+from .model import GTFS2VecModel
+
+__all__ = ["GTFS2VecEmbedder", "GTFS2VecModel"]
