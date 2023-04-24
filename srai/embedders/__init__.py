90c63d0469b21e8fb556e738f47dc2c3d8d2e4c2
Piotr <p.gramacki@gmail.com>
2023-04-24 18:28:37 +0200

    docs: add module descriptions

--- srai/embedders/__init__.py
+++ srai/embedders/__init__.py
@@ -1,4 +1,10 @@
-"""Embedders."""
+"""
+This module contains embedders, used to convert spatial data to their vector representations.
+
+Embedders are designed to unify different types of spatial data embedding methods, such as hex2vec
+or gtfs2vec into a single interface. This allows to easily switch between different embedding
+methods without changing the rest of the code.
+"""
 
 from ._base import Embedder
 from .count_embedder import CountEmbedder
--- srai/utils/__init__.py
+++ srai/utils/__init__.py
@@ -1,4 +1,9 @@
-"""Utilities."""
+"""
+Module containing different utility functions.
+
+Those are either used internally by other modules, or can be used to simplify spatial data
+processing.
+"""
 # flake8: noqa F401
 from .download import *
 from .geocode import *
