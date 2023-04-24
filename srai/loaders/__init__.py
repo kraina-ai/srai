b3a3d21b054608f1386aeb10087674bccc5ad2b6
Kamil Raczycki <raczyckikamil@gmail.com>
2023-04-24 22:31:30 +0200

    docs: modify mkdocs rendering

--- srai/loaders/__init__.py
+++ srai/loaders/__init__.py
@@ -9,6 +9,14 @@ methods available in this library.
 from ._base import Loader
 from .geoparquet_loader import GeoparquetLoader
 from .gtfs_loader import GTFSLoader
+from .osm_loaders import OSMOnlineLoader, OSMPbfLoader
 from .osm_way_loader import OSMWayLoader
 
-__all__ = ["Loader", "GeoparquetLoader", "GTFSLoader", "OSMWayLoader"]
+__all__ = [
+    "Loader",
+    "GeoparquetLoader",
+    "GTFSLoader",
+    "OSMWayLoader",
+    "OSMOnlineLoader",
+    "OSMPbfLoader",
+]
