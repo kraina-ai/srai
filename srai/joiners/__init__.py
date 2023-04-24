4729d649180d3cbb63f243631080210ab983f6ac
Calychas <kacper.lesniara@gmail.com>
2023-04-24 18:36:39 +0200

    fix: typo

--- srai/joiners/__init__.py
+++ srai/joiners/__init__.py
@@ -1,7 +1,7 @@
 """
 This module contains joiners, used to match spatial data with regions used in analysis.
 
-All mathods for spatial embedding (available in `srai`) are designed to operate on regions, not on
+All methods for spatial embedding (available in `srai`) are designed to operate on regions, not on
 individual spatial features. This means that we need to match spatial features with our given
 regions. This can be done in different ways, all of which are available under a common `Joiner`
 interface.
