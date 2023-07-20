a80d47c2568c6a8b3a30ab961083fedc4d0460a2
Kamil Raczycki <raczyckikamil@gmail.com>
2023-07-20 14:53:33 +0200

    refactor: remove geocode module from utils

--- srai/utils/__init__.py
+++ srai/utils/__init__.py
@@ -4,12 +4,10 @@ Module containing different utility functions.
 Those are either used internally by other modules, or can be used to simplify spatial data
 processing.
 """
-from .geocode import geocode_to_region_gdf
 from .geometry import buffer_geometry, flatten_geometry, flatten_geometry_series, remove_interiors
 from .merge import merge_disjointed_gdf_geometries, merge_disjointed_polygons
 
 __all__ = [
-    "geocode_to_region_gdf",
     "buffer_geometry",
     "flatten_geometry",
     "flatten_geometry_series",
