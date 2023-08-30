48c6c67fc3cea02c0f95d6c6eb445b22216d1f31
Kamil Raczycki <raczyckikamil@gmail.com>
2023-08-30 10:00:54 +0200

    refactor: remove h3ronpy macos override (#356)
    * chore: remove h3ronpy macos override
    
    * chore: added changelog entry

--- srai/h3.py
+++ srai/h3.py
@@ -1,6 +1,5 @@
 """Utility H3 related functions."""
 
-from sys import platform
 from typing import Iterable, List, Literal, Tuple, Union, overload
 
 import geopandas as gpd
@@ -8,11 +7,10 @@ import h3
 import numpy as np
 import numpy.typing as npt
 from h3ronpy.arrow.vector import cells_to_wkb_polygons, wkb_to_cells
-from shapely.geometry import Point, Polygon
+from shapely.geometry import Polygon
 from shapely.geometry.base import BaseGeometry
 
 from srai.constants import GEOMETRY_COLUMN, WGS84_CRS
-from srai.geometry import buffer_geometry
 
 __all__ = [
     "shapely_geometry_to_h3",
@@ -49,9 +47,6 @@ def shapely_geometry_to_h3(
     if not (0 <= h3_resolution <= 15):
         raise ValueError(f"Resolution {h3_resolution} is not between 0 and 15.")
 
-    if _is_macos():
-        return _polygon_to_h3_index_raw(geometry, h3_resolution, buffer)
-
     wkb = []
     if isinstance(geometry, gpd.GeoSeries):
         wkb = geometry.to_wkb()
@@ -69,64 +64,6 @@ def shapely_geometry_to_h3(
     return [h3.int_to_str(h3_index) for h3_index in h3_indexes.tolist()]
 
 
-def _polygon_to_h3_index_raw(
-    geometry: Union[BaseGeometry, Iterable[BaseGeometry], gpd.GeoSeries, gpd.GeoDataFrame],
-    h3_resolution: int,
-    buffer: bool = True,
-) -> List[str]:
-    def _polygon_shapely_to_h3(
-        geometry: Union[Point, Polygon], h3_resolution: int, buffer: bool
-    ) -> List[str]:
-        if isinstance(geometry, Point):
-            return [h3.latlng_to_cell(geometry.y, geometry.x, h3_resolution)]
-
-        buffer_distance_meters = 2 * h3.average_hexagon_edge_length(h3_resolution, unit="m")
-
-        buffered_geometry = (
-            buffer_geometry(geometry, buffer_distance_meters) if buffer else geometry
-        )
-
-        exterior = [coord[::-1] for coord in list(buffered_geometry.exterior.coords)]
-        interiors = [
-            [coord[::-1] for coord in list(interior.coords)]
-            for interior in buffered_geometry.interiors
-        ]
-        h3_cells: List[str] = h3.polygon_to_cells(h3.Polygon(exterior, *interiors), h3_resolution)
-
-        if buffer:
-            h3_cells = [
-                h3_cell
-                for h3_cell in h3_cells
-                if _h3_index_to_polygon_raw(h3_cell).intersects(geometry)
-            ]
-
-        return h3_cells
-
-    from functional import seq
-
-    geoseries: gpd.GeoSeries
-
-    if isinstance(geometry, gpd.GeoSeries):
-        geoseries = geometry
-    elif isinstance(geometry, gpd.GeoDataFrame):
-        geoseries = geometry[GEOMETRY_COLUMN]
-    elif isinstance(geometry, Iterable):
-        geoseries = gpd.GeoSeries(geometry, crs=WGS84_CRS)
-    else:
-        return _polygon_to_h3_index_raw([geometry], h3_resolution, buffer)
-
-    geoseries = geoseries.explode(ignore_index=True, index_parts=True)
-
-    h3_indexes: List[str] = (
-        seq(geoseries)
-        .flat_map(lambda polygon: _polygon_shapely_to_h3(polygon, h3_resolution, buffer))
-        .distinct()
-        .to_list()
-    )
-
-    return h3_indexes
-
-
 # TODO: write tests (#322)
 def h3_to_geoseries(h3_index: Union[int, str, Iterable[Union[int, str]]]) -> gpd.GeoSeries:
     """
@@ -142,26 +79,10 @@ def h3_to_geoseries(h3_index: Union[int, str, Iterable[Union[int, str]]]) -> gpd
     if isinstance(h3_index, (str, int)):
         return h3_to_geoseries([h3_index])
     else:
-        if _is_macos():
-            return gpd.GeoSeries(
-                [_h3_index_to_polygon_raw(h3_cell) for h3_cell in h3_index], crs=WGS84_CRS
-            )
-
         h3_int_indexes = (
             h3_cell if isinstance(h3_cell, int) else h3.str_to_int(h3_cell) for h3_cell in h3_index
         )
         return gpd.GeoSeries.from_wkb(cells_to_wkb_polygons(h3_int_indexes), crs=WGS84_CRS)
-
-
-def _h3_index_to_polygon_raw(h3_index: Union[int, str]) -> Polygon:
-    if isinstance(h3_index, int):
-        h3_index = h3.int_to_str(h3_index)
-    h3_poly = h3.cells_to_polygons([h3_index])[0]
-
-    return Polygon(
-        shell=[coord[::-1] for coord in h3_poly.outer],
-        holes=[[coord[::-1] for coord in hole] for hole in h3_poly.holes],
-    )
 
 
 @overload
@@ -252,8 +173,3 @@ def get_local_ij_index(
         local_ijs = [(coords[0], coords[1]) for coords in local_ijs]
 
     return local_ijs
-
-
-def _is_macos() -> bool:
-    """Return flag if code is run on OS X."""
-    return platform == "darwin"
