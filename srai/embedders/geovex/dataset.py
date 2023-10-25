fee892c823adb83f7b0a675d76ab23564125016c
Kamil Raczycki <raczyckikamil@gmail.com>
2023-10-25 09:02:16 +0200

    ci: change logging level

--- srai/embedders/geovex/dataset.py
+++ srai/embedders/geovex/dataset.py
@@ -93,7 +93,7 @@ class HexagonalDataset(Dataset["torch.Tensor"], Generic[T]):  # type: ignore
             neighbors = neighbourhood.get_neighbours_up_to_distance(
                 h3_index, neighbor_k_ring, include_center=False, unchecked=True
             )
-            LOGGER.info(f"{h3_index}: {neighbors}")
+            LOGGER.warn(f"{h3_index}: {neighbors}")
             # check if all the neighbors are in the dataset
             if len(neighbors.intersection(all_indices)) == len(neighbors):
                 # add the h3_index to the valid h3 indices, with the ring of neighbors
@@ -111,7 +111,7 @@ class HexagonalDataset(Dataset["torch.Tensor"], Generic[T]):  # type: ignore
             else:
                 # some of the neighbors are not in the dataset, add the h3_index to the invalid h3s
                 invalid_h3s.add(h3_index)
-        LOGGER.info(valid_h3s)
+        LOGGER.warn(valid_h3s)
         return invalid_h3s, valid_h3s
 
     def __len__(self) -> int:
