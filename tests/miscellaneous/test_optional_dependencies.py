10b811b2b3cb16ebf734a2de5ddb8c3d11211d8e
Calychas <kacper.lesniara@gmail.com>
2023-10-25 20:08:11 +0200

    test: add GeoVexEmbedder to optional dependencies

--- tests/miscellaneous/test_optional_dependencies.py
+++ tests/miscellaneous/test_optional_dependencies.py
@@ -95,11 +95,17 @@ def _test_plotting() -> None:
 
 
 def _test_torch() -> None:
-    from srai.embedders import GTFS2VecEmbedder, Hex2VecEmbedder, Highway2VecEmbedder
+    from srai.embedders import (
+        GeoVexEmbedder,
+        GTFS2VecEmbedder,
+        Hex2VecEmbedder,
+        Highway2VecEmbedder,
+    )
 
     Highway2VecEmbedder()
     GTFS2VecEmbedder()
     Hex2VecEmbedder()
+    GeoVexEmbedder(["a"] * 256)
 
 
 def _test_osm() -> None:
