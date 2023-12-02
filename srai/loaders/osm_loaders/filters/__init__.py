41c70780e02e46a5c5d617b52472cbc27ee8e57b
Kamil Raczycki <raczyckikamil@gmail.com>
2023-12-02 19:30:28 +0100

    feat: add more options to merge OsmTagsFilters

--- srai/loaders/osm_loaders/filters/__init__.py
+++ srai/loaders/osm_loaders/filters/__init__.py
@@ -3,7 +3,6 @@
 from ._typing import (
     GroupedOsmTagsFilter,
     OsmTagsFilter,
-    merge_grouped_osm_tags_filter,
     merge_osm_tags_filter,
 )
 from .base_osm_groups import BASE_OSM_GROUPS_FILTER
@@ -15,7 +14,6 @@ __all__ = [
     "GroupedOsmTagsFilter",
     "OsmTagsFilter",
     "merge_osm_tags_filter",
-    "merge_grouped_osm_tags_filter",
     "BASE_OSM_GROUPS_FILTER",
     "GEOFABRIK_LAYERS",
     "HEX2VEC_FILTER",
--- tests/loaders/osm_loaders/filters/test_merge_filter_types.py
+++ tests/loaders/osm_loaders/filters/test_merge_filter_types.py
@@ -1,43 +1,33 @@
 """Tests for merging OSM Loaders filters."""
-from contextlib import nullcontext as does_not_raise
 from typing import Any
 from unittest import TestCase
 
 import pytest
 
-from srai.loaders.osm_loaders.filters import OsmTagsFilter, merge_grouped_osm_tags_filter
+from srai.loaders.osm_loaders.filters import OsmTagsFilter, merge_osm_tags_filter
 
 ut = TestCase()
 
 
 @pytest.mark.parametrize(  # type: ignore
-    "grouped_filter,expected_result_filter,expectation",
+    "osm_tags_filter,expected_result_filter",
     [
-        ({"tag_a": True}, {"tag_a": True}, pytest.raises(ValueError)),
-        ({"tag_a": "A"}, {"tag_a": "A"}, pytest.raises(ValueError)),
-        ({"tag_a": ["A"]}, {"tag_a": ["A"]}, pytest.raises(ValueError)),
-        ({}, {}, does_not_raise()),
-        ({"group_a": {}}, {}, does_not_raise()),
-        ({"group_a": {"tag_a": True}}, {"tag_a": True}, does_not_raise()),
-        ({"group_a": {"tag_a": "A"}}, {"tag_a": ["A"]}, does_not_raise()),
-        ({"group_a": {"tag_a": ["A"]}}, {"tag_a": ["A"]}, does_not_raise()),
-        (
-            {"group_a": {"tag_a": "A", "tag_b": "B"}},
-            {"tag_a": ["A"], "tag_b": ["B"]},
-            does_not_raise(),
-        ),
-        (
-            {"group_a": {"tag_a": ["A"], "tag_b": ["B"]}},
-            {"tag_a": ["A"], "tag_b": ["B"]},
-            does_not_raise(),
-        ),
+        ({"tag_a": True}, {"tag_a": True}),
+        ({"tag_a": "A"}, {"tag_a": "A"}),
+        ({"tag_a": ["A"]}, {"tag_a": ["A"]}),
+        ({}, {}),
+        ({"group_a": {}}, {}),
+        ({"group_a": {"tag_a": True}}, {"tag_a": True}),
+        ({"group_a": {"tag_a": "A"}}, {"tag_a": ["A"]}),
+        ({"group_a": {"tag_a": ["A"]}}, {"tag_a": ["A"]}),
+        ({"group_a": {"tag_a": "A", "tag_b": "B"}}, {"tag_a": ["A"], "tag_b": ["B"]}),
+        ({"group_a": {"tag_a": ["A"], "tag_b": ["B"]}}, {"tag_a": ["A"], "tag_b": ["B"]}),
         (
             {
                 "group_a": {"tag_a": "A", "tag_b": "B"},
                 "group_b": {"tag_a": "A", "tag_b": "B"},
             },
             {"tag_a": ["A"], "tag_b": ["B"]},
-            does_not_raise(),
         ),
         (
             {
@@ -45,7 +35,6 @@ ut = TestCase()
                 "group_b": {"tag_c": "C", "tag_d": "D"},
             },
             {"tag_a": ["A"], "tag_b": ["B"], "tag_c": ["C"], "tag_d": ["D"]},
-            does_not_raise(),
         ),
         (
             {
@@ -53,7 +42,6 @@ ut = TestCase()
                 "group_b": {"tag_a": "C", "tag_b": "D"},
             },
             {"tag_a": ["A", "C"], "tag_b": ["B", "D"]},
-            does_not_raise(),
         ),
         (
             {
@@ -61,7 +49,6 @@ ut = TestCase()
                 "group_b": {"tag_a": ["C", "D"], "tag_b": "E"},
             },
             {"tag_a": ["A", "C", "D"], "tag_b": ["B", "E"]},
-            does_not_raise(),
         ),
         (
             {
@@ -69,7 +56,6 @@ ut = TestCase()
                 "group_b": {"tag_a": ["C", "D"], "tag_b": True},
             },
             {"tag_a": ["A", "C", "D"], "tag_b": True},
-            does_not_raise(),
         ),
         (
             {
@@ -77,7 +63,6 @@ ut = TestCase()
                 "group_b": {"tag_a": ["C", "D"], "tag_b": False},
             },
             {"tag_a": ["A", "C", "D"], "tag_b": ["B"]},
-            does_not_raise(),
         ),
         (
             {
@@ -85,14 +70,51 @@ ut = TestCase()
                 "group_b": {"tag_a": ["C", "D"], "tag_b": ["B"]},
             },
             {"tag_a": ["A", "C", "D"], "tag_b": ["B", "E"]},
-            does_not_raise(),
+        ),
+        ([{"tag_a": True}], {"tag_a": True}),
+        ([{"tag_a": "A"}], {"tag_a": ["A"]}),
+        ([{"tag_a": ["A"]}], {"tag_a": ["A"]}),
+        ([{}], {}),
+        ([{"group_a": {}}], {}),
+        (
+            [{"tag_a": "A", "tag_b": "B"}, {"tag_a": "A", "tag_b": "B"}],
+            {"tag_a": ["A"], "tag_b": ["B"]},
+        ),
+        (
+            [
+                {
+                    "group_a": {"tag_a": "A", "tag_b": "B"},
+                    "group_b": {"tag_a": "A", "tag_b": "B"},
+                },
+                {"tag_a": "A", "tag_b": "B"},
+            ],
+            {"tag_a": ["A"], "tag_b": ["B"]},
+        ),
+        (
+            [
+                {
+                    "group_a": {"tag_a": "A", "tag_b": "B"},
+                    "group_b": {"tag_a": "A", "tag_b": "B"},
+                },
+                {
+                    "group_a": {"tag_a": "A", "tag_b": "B"},
+                    "group_b": {"tag_a": "A", "tag_b": "B"},
+                },
+            ],
+            {"tag_a": ["A"], "tag_b": ["B"]},
+        ),
+        ([{}, {}], {}),
+        ([{"group_a": {}}, {"group_a": {}}], {}),
+        ([{"group_a": {}}, {}], {}),
+        (
+            [{"tag_a": "A", "tag_b": "B"}, {"tag_c": "C", "tag_d": "D"}],
+            {"tag_a": ["A"], "tag_b": ["B"], "tag_c": ["C"], "tag_d": ["D"]},
         ),
     ],
 )
-def test_merge_grouped_filters(
-    grouped_filter: Any, expected_result_filter: OsmTagsFilter, expectation: Any
+def test_merge_osm_tags_filters(
+    osm_tags_filter: Any, expected_result_filter: OsmTagsFilter
 ) -> None:
     """Test merging grouped tags filter into a base osm filter."""
-    with expectation:
-        merged_filters = merge_grouped_osm_tags_filter(grouped_filter)
-        ut.assertDictEqual(expected_result_filter, merged_filters)
+    merged_filters = merge_osm_tags_filter(osm_tags_filter)
+    ut.assertDictEqual(expected_result_filter, merged_filters)
