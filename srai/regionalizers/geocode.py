4f6aaa9c3dd194d8e8537c16b8edcfceee5b0f26
Kamil Raczycki <raczyckikamil@gmail.com>
2023-12-06 18:55:32 +0100

    chore: change geocode_to_region_gdf doctests

--- srai/regionalizers/geocode.py
+++ srai/regionalizers/geocode.py
@@ -33,25 +33,25 @@ def geocode_to_region_gdf(
         Download geometry for a city
         >>> from srai.regionalizers import geocode_to_region_gdf
         >>> geocode_to_region_gdf("Wrocław, PL")
-                                                          geometry
+                           geometry
         region_id
-        Wrocław, Lower Silesian Voivodeship, Poland  POLYGON ((...
+        Wrocław...    POLYGON ((...
 
         Download geometries for multiple cities
 
         >>> geocode_to_region_gdf(["New York City", "Washington, DC"])
-                                                                    geometry
+                                  geometry
         region_id
-        New York, United States                          MULTIPOLYGON (((...
-        Washington, District of Columbia, United States  POLYGON ((...
+        New York...    MULTIPOLYGON (((...
+        Washington...        POLYGON ((...
 
         Use OSM relation IDs to get geometries.
 
         >>> geocode_to_region_gdf(["R175342", "R5750005"], by_osmid=True)
-                                                                 geometry
+                                  geometry
         region_id
-        Greater London, England, United Kingdom             POLYGON ((...
-        Sydney, Council of the City of Sydney, New Sout...  POLYGON ((...
+        Greater London...    POLYGON ((...
+        Sydney...            POLYGON ((...
     """
     import_optional_dependencies(
         dependency_group="osm",
