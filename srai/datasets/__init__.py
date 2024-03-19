a9926935c36e65975734bef03510dcf541b162c4
Repcak2000 <276940@student.pwr.edu.pl>
2024-03-19 12:38:31 +0100

    Brightkite dataset added.

--- srai/datasets/__init__.py
+++ srai/datasets/__init__.py
@@ -2,5 +2,6 @@ from ._base import Dataset
 from .geolife import Geolife
 from .house_sales_in_king_country import HouseSalesInKingCountry
 from .nyc_bike import NYCBike
+from .brightkite import Brightkite
 
-__all__ = ["Dataset", "NYCBike", "Geolife", "HouseSalesInKingCountry"]
+__all__ = ["Dataset", "NYCBike", "Geolife", "HouseSalesInKingCountry", "Brightkite"]
