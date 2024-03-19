eb169cf1d11191902230dc9b0c939b50c3554091
Repcak2000 <276940@student.pwr.edu.pl>
2024-03-19 17:41:47 +0100

    Philadelphia Crime dataset addition

--- srai/datasets/__init__.py
+++ srai/datasets/__init__.py
@@ -3,5 +3,13 @@ from .brightkite import Brightkite
 from .geolife import Geolife
 from .house_sales_in_king_country import HouseSalesInKingCountry
 from .nyc_bike import NYCBike
+from .philadelphia_crime import PhiladelphiaCrime
 
-__all__ = ["Dataset", "NYCBike", "Geolife", "HouseSalesInKingCountry", "Brightkite"]
+__all__ = [
+    "Dataset",
+    "NYCBike",
+    "Geolife",
+    "HouseSalesInKingCountry",
+    "Brightkite",
+    "PhiladelphiaCrime",
+]
---
+++ srai/datasets/philadelphia_crime.py
@@ -0,0 +1,22 @@
+from srai.datasets import Dataset
+import geopandas as gpd
+
+
+class PhiladelphiaCrime(Dataset):
+    def _preprocessing(self, data: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+        """
+        Preprocessing to get GeoDataFrame with location data, based on GEO_EDA files.
+
+        Args:
+            data: Data of Philadelphia Crime dataset.
+
+        Returns:
+            GeoDataFrame of dataset, contatins location data.
+        """
+        df = data.copy()
+        gdf = gpd.GeoDataFrame(
+            df.drop(["lng", "lat"], axis=1),
+            geometry=gpd.points_from_xy(df["lng"], df["lat"]),
+            crs="EPSG:4326",
+        )
+        return gdf
