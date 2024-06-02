9653cc56f7e2e5ab51135af55e74cb85c46ec66e
Julia Moska <j.moskaaa@gmail.com>
2024-06-02 13:52:22 +0200

    added constant to metric calculation to avoid zeroes

--- srai/models/evaluator/metrics.py
+++ srai/models/evaluator/metrics.py
@@ -8,17 +8,20 @@ import numpy as np
 import torch
 
 
-def mean_absolute_percentage_error(y_true: torch.Tensor, y_pred: torch.Tensor) -> np.ndarray:
+def mean_absolute_percentage_error(
+    y_true: torch.Tensor, y_pred: torch.Tensor, epsilon: float = 1e-10
+) -> np.ndarray:
     """Calculates regression metric: Mean Absolute Percentage Error.
 
     Args:
         y_true (torch.Tensor): Expected values
         y_pred (torch.Tensor): Predicted values
+        epsilon (float): Small constant to avoid division by zero (default: 1e-10)
 
     Returns:
         np.ndarray: mean absolute percentage error value
     """
-    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+    return np.mean(np.abs((y_true - y_pred) / y_true + epsilon)) * 100
 
 
 def symmetric_mean_absolute_percentage_error(
