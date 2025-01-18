bfbd5ee98661796ce0766725dc397b2de87a2ec2
Julia Moska <j.moskaaa@gmail.com>
2025-01-18 12:41:17 +0100

    feat: deleted unneccesary functions related to h3 labels retrieval from evaluator

--- srai/benchmark/_base.py
+++ srai/benchmark/_base.py
@@ -4,11 +4,9 @@ import abc
 import logging
 from typing import Literal, Optional
 
-import geopandas as gpd
 import numpy as np
 
 import srai.datasets as sds
-from srai.regionalizers import H3Regionalizer
 
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 
@@ -46,9 +44,19 @@ class Evaluator(abc.ABC):
             dict[str, float]: Dictionary with metrics values for the task.
         """
         if self.task == "regression":
-            _, test = dataset.load(version=f"res_{resolution}", hf_token=hf_token)
-            target_column = dataset.target
-            h3_indexes, labels = self._get_labels(test, resolution, target_column)
+            train_gdf, test_gdf = dataset.load(version=f"res_{resolution}", hf_token=hf_token)
+            target_column = dataset.target if dataset.target is not None else "count"
+            # h3_indexes, labels = self._get_labels(test, resolution, target_column)
+            _, h3_test = dataset.get_h3_with_labels(
+                train_gdf=train_gdf, test_gdf=test_gdf, resolution=resolution
+            )
+
+            if h3_test is None:
+                raise ValueError("The function 'get_h3_with_labels' returned None for h3_test.")
+            else:
+                h3_indexes = h3_test["region_id"].to_list()
+                labels = h3_test[target_column].to_numpy()
+
             region_to_prediction = {
                 region_id: prediction for region_id, prediction in zip(region_ids, predictions)
             }
@@ -67,32 +75,6 @@ class Evaluator(abc.ABC):
             if log_metrics:
                 self._log_metrics(metrics)
             return metrics
-        else:
-            raise NotImplementedError
-
-    def get_evaluation_info(
-        self,
-        dataset: sds.HuggingFaceDataset,
-        resolution: int,
-        hf_token: Optional[str] = None,
-    ) -> list[str]:
-        """
-        Retrieves the H3 indexes in the test set for a given benchmark.
-
-        Args:
-            dataset (sds.HuggingFaceDataset): Dataset to evaluate.
-            resolution (int): Resolution of the H3 grid.
-            hf_token (str, optional): If needed, a User Access Token needed to authenticate to HF
-                Defaults to None.
-
-        Returns:
-            list[str]: List of H3 indexes in the test set.
-        """
-        if self.task == "regression":
-            _, test = dataset.load(version=f"res_{resolution}", hf_token=hf_token)
-            target_column = dataset.target
-            h3_indexes, _ = self._get_labels(test, resolution, target_column)
-            return h3_indexes
         else:
             raise NotImplementedError
 
@@ -129,25 +111,3 @@ class Evaluator(abc.ABC):
         logging.info("Resulting metrics: ")
         for key, value in metrics.items():
             logging.info(f"{key}: {value:.4f}")
-
-    def _get_labels(
-        self, test_gdf: gpd.GeoDataFrame, resolution: int, target_column: Optional[str]
-    ) -> tuple[list[str], np.array]:
-        """Returns labels from the dataset."""
-        if self.task == "regression":
-            if target_column is None:
-                target_column = "count"
-            gdf_ = test_gdf.copy()
-            regionalizer = H3Regionalizer(resolution=resolution)
-            regions = regionalizer.transform(test_gdf)
-            joined_gdf = gpd.sjoin(test_gdf, regions, how="left", predicate="within")  # noqa: E501
-            joined_gdf.rename(columns={"index_right": "h3_index"}, inplace=True)
-
-            averages_hex = joined_gdf.groupby("h3_index").size().reset_index(name=target_column)
-            gdf_ = regions.merge(
-                averages_hex, how="inner", left_on="region_id", right_on="h3_index"
-            )
-            return gdf_["h3_index"].to_list(), gdf_[target_column].to_numpy()
-
-        else:
-            raise NotImplementedError
