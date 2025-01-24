9c34e9c9a03a0837ad89d156fa008293830fc198
Kamil Raczycki <raczyckikamil@gmail.com>
2025-01-24 22:55:56 +0100

    feat: add google colab link generation for docs

--- docs/copy_examples.py
+++ docs/copy_examples.py
@@ -3,8 +3,22 @@
 from pathlib import Path
 
 import mkdocs_gen_files
+import nbformat
 
 EXAMPLES_DIRECTORY_PATH = Path("examples")
+
+RUN_IN_COLAB_CELL_MARKDOWN = """
+Run this notebook in Google Colab:
+
+<a target="_blank" href="https://colab.research.google.com/github/kraina-ai/srai/blob/main/{relative_file_path}">
+  <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+</a>
+
+Remember to install the `srai` library before running the notebook:
+```bash
+!pip install srai[all]
+```
+"""
 
 
 def write_file(file_path: Path) -> None:
@@ -16,8 +30,29 @@ def write_file(file_path: Path) -> None:
     """
     root_path = file_path.relative_to(".")
     print(f"Copying {root_path} file to {root_path}")
-    with root_path.open("rb") as src, mkdocs_gen_files.open(root_path, "wb") as dst:
-        dst.write(src.read())
+    with root_path.open("r") as src, mkdocs_gen_files.open(root_path, "w") as dst:
+        if root_path.suffix != ".ipynb":
+            dst.write(src.read())
+            return
+
+        nb = nbformat.read(src, as_version=4)
+        has_google_colab_link = any(
+            "colab.research.google.com" in cell.get("source", "") for cell in nb["cells"]
+        )
+        if not has_google_colab_link:
+            for i, cell in enumerate(nb["cells"]):
+                if cell["cell_type"] == "code":
+                    nb["cells"][i:] = (
+                        nbformat.v4.new_markdown_cell(
+                            RUN_IN_COLAB_CELL_MARKDOWN.format(relative_file_path=root_path)
+                        ),
+                        *nb["cells"][i:-1],
+                    )
+                    break
+        else:
+            print("Google Colab link already present in the notebook.")
+
+        nbformat.write(nb, dst)
 
 
 banned_directories = [
