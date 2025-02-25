207b56fd128bedcc4869a0f0ca47791a7e0016f7
Julia Moska <j.moskaaa@gmail.com>
2025-02-25 18:25:56 +0100

    feat: Changed all methods in base evaluator to abstract to ensure flexibility. Adjusted accordingly code in HexRegressionEvaluator

--- srai/benchmark/__init__.py
+++ srai/benchmark/__init__.py
@@ -1,6 +1,6 @@
 """The benchmark module contains classes for evaluating the performance of a model on a dataset."""
 
-from ._base import Evaluator
-from .regression_evaluator import RegressionEvaluator
+from ._base import BaseEvaluator
+from .regression_evaluator import HexRegressionEvaluator
 
-__all__ = ["Evaluator", "RegressionEvaluator"]
+__all__ = ["BaseEvaluator", "HexRegressionEvaluator"]
--- srai/benchmark/regression_evaluator.py
+++ srai/benchmark/regression_evaluator.py
@@ -1,9 +1,12 @@
-"""This module contains RegressionEvaluator dataset."""
+"""This module contains HexRegressionEvaluator dataset."""
+
+from typing import Any, Optional
 
 import numpy as np
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-from srai.benchmark import Evaluator
+import srai.datasets as sds
+from srai.benchmark import BaseEvaluator
 
 from ._custom_metrics import (
     mean_absolute_percentage_error,
@@ -11,12 +14,75 @@ from ._custom_metrics import (
 )
 
 
-class RegressionEvaluator(Evaluator):
+class HexRegressionEvaluator(BaseEvaluator):
     """Evaluator for regression task."""
 
     def __init__(self) -> None:
         """Create the evaluator."""
         super().__init__(task="regression")
+
+    def evaluate(
+        self,
+        dataset: sds.HuggingFaceDataset,
+        predictions: np.ndarray,
+        log_metrics: bool = True,
+        hf_token: Optional[str] = None,
+        **kwargs: Any,
+    ) -> dict[str, float]:
+        """
+        Evaluate regression predictions against test set.
+
+        This regression evaluator is designed for H3 grid predictions.
+
+        Args:
+            dataset (sds.HuggingFaceDataset): Dataset to evaluate.
+            predictions (np.ndarray): Predictions returned by your model. Should match regions_id.
+            log_metrics (bool, optional): If True, logs metrics to the console. Defaults to True.
+            hf_token (str, optional): If needed, a User Access Token needed to authenticate to HF
+                Defaults to None.
+            **kwargs: Additional keyword arguments.
+
+        Keyword Args:
+            region_ids (list[str]): List of region IDs. Required for region-based evaluators.
+
+        Raises:
+            ValueError: If region id for H3 index not found in region_ids.
+
+        Returns:
+            dict[str, float]: Dictionary with metrics values for the task.
+        """
+        region_ids = kwargs.get("region_ids")
+
+        if region_ids is None:
+            raise ValueError("Region_ids are required for region-based evaluation.")
+
+        target_column = dataset.target if dataset.target is not None else "count"
+        _, h3_test = dataset.get_h3_with_labels()
+
+        if h3_test is None:
+            raise ValueError("The function 'get_h3_with_labels' returned None for h3_test.")
+        else:
+            h3_indexes = h3_test["region_id"].to_list()
+            labels = h3_test[target_column].to_numpy()
+
+        region_to_prediction = {
+            region_id: prediction for region_id, prediction in zip(region_ids, predictions)
+        }
+
+        # order predictions according to the order of region_ids
+        try:
+            ordered_predictions = [region_to_prediction[h3] for h3 in h3_indexes]
+        except KeyError as err:
+            raise ValueError(
+                "Region id for H3 index {err.args[0]} not found in region_ids."
+            ) from err
+
+        region_ids[:] = h3_indexes
+        predictions = np.array(ordered_predictions)
+        metrics = self._compute_metrics(predictions, labels)
+        if log_metrics:
+            self._log_metrics(metrics)
+        return metrics
 
     def _compute_metrics(self, predictions: np.ndarray, labels: np.ndarray) -> dict[str, float]:
         """
