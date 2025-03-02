ee158737b2b1527bd1ded82c09ced886063e09af
Kamil Raczycki <raczyckikamil@gmail.com>
2025-03-02 19:32:24 +0100

    chore: add benchmark test for memory usage

---
+++ tests/benchmark/test_large_area_embedding.py
@@ -0,0 +1,55 @@
+"""Test for checking memory usage of the whole pipeline."""
+
+import random
+from random import choice
+from string import ascii_lowercase, digits
+
+import geopandas as gpd
+import numpy as np
+from shapely import box
+
+from srai.constants import FEATURES_INDEX
+from srai.embedders import ContextualCountEmbedder
+from srai.h3 import ring_buffer_h3_regions_gdf
+from srai.joiners import IntersectionJoiner
+from srai.neighbourhoods import H3Neighbourhood
+from srai.regionalizers import H3Regionalizer, S2Regionalizer
+
+
+def test_large_area_embedding() -> None:
+    """Test if large area embedding can be calculated on a single machine."""
+    H3_RESOLUTION = 5
+    S2_RESOLUTION = 9
+    H3_DISTANCE = 10
+
+    chars = ascii_lowercase + digits
+    columns = ["".join(choice(chars) for _ in range(8)) for _ in range(100)]
+    values = ["".join(choice(chars) for _ in range(8)) for _ in range(100)]
+
+    area = gpd.GeoDataFrame(geometry=[box(5.818355, 46.037418, 24.363277, 52.769854)], crs=4326)
+
+    h3_regions = H3Regionalizer(resolution=H3_RESOLUTION).transform(area)
+    print(f"H3 regions: {len(h3_regions)}")
+    buffered_h3_regions = ring_buffer_h3_regions_gdf(h3_regions, H3_DISTANCE)
+    print(f"Buffered H3 regions: {len(buffered_h3_regions)}")
+
+    s2_regions = S2Regionalizer(resolution=S2_RESOLUTION).transform(area)
+    print(f"S2 regions: {len(s2_regions)}")
+
+    data = np.full((len(s2_regions), len(columns)), None)
+    for i in range(len(s2_regions)):
+        data[i, random.randint(0, len(columns) - 1)] = random.choice(values)
+
+    s2_regions[columns] = data
+    s2_regions.index.rename(FEATURES_INDEX, inplace=True)
+
+    joint = IntersectionJoiner().transform(buffered_h3_regions, s2_regions)
+    print(f"Joint: {len(joint)}")
+
+    embeddings = ContextualCountEmbedder(
+        neighbourhood=H3Neighbourhood(),
+        neighbourhood_distance=H3_DISTANCE,
+        count_subcategories=True,
+    ).transform(buffered_h3_regions, s2_regions, joint)
+
+    assert len(embeddings) == len(buffered_h3_regions)
