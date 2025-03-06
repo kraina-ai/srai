52338a533a48b53ef815820f7d7a1db42be7793a
Kamil Raczycki <raczyckikamil@gmail.com>
2025-03-06 22:11:03 +0100

    feat: add geometry ordering and unary operator

--- srai/geodatatable.py
+++ srai/geodatatable.py
@@ -2,11 +2,14 @@
 
 import hashlib
 import inspect
+import multiprocessing
 import weakref
-from collections.abc import Iterable, Sized
+from collections.abc import Callable, Iterable, Sized
+from concurrent.futures import ProcessPoolExecutor
 from datetime import datetime
+from functools import partial
 from pathlib import Path
-from typing import TYPE_CHECKING, Optional, Union, cast
+from typing import TYPE_CHECKING, Literal, Optional, Union, cast
 
 import duckdb
 import geopandas as gpd
@@ -14,7 +17,14 @@ import pandas as pd
 import pyarrow.dataset as ds
 import pyarrow.parquet as pq
 from geoarrow.pyarrow.io import _geoparquet_guess_geometry_columns
+from geoarrow.rust.core import (
+    ChunkedGeometryArray,
+    to_shapely,
+)
 from psutil._common import bytes2human
+from shapely import coverage_union_all, union_all
+from shapely.geometry.base import BaseGeometry
+from tqdm import tqdm
 
 from srai.constants import GEOMETRY_COLUMN
 from srai.duckdb import prepare_duckdb_extensions
@@ -25,6 +35,9 @@ if TYPE_CHECKING:
 from typing import TypeVar
 
 _Self = TypeVar("_Self", bound="ParquetDataTable")
+
+PARQUET_ROW_GROUP_SIZE = 100_000
+PARQUET_COMPRESSION = "zstd"
 
 
 class ParquetDataTable(Sized):
@@ -64,9 +77,11 @@ class ParquetDataTable(Sized):
             else None
         )
         self.parquet_paths = parquet_paths
-        self._finalizer = None
-        if not persist_files:
-            self._finalizer = weakref.finalize(self, self._cleanup_files, self.parquet_paths)
+        self.persist_files = persist_files
+        self._finalizer: Optional[weakref.finalize] = None
+
+        if not self.persist_files:
+            self._configure_finalizer()
 
     @property
     def index_name(self) -> Optional[str]:
@@ -131,6 +146,14 @@ class ParquetDataTable(Sized):
         print("cleanup!")
         for path in paths:
             path.unlink(missing_ok=True)
+
+    def _detach_finalizer(self) -> None:
+        if self._finalizer is not None:
+            self._finalizer.detach()
+
+    def _configure_finalizer(self) -> None:
+        self._detach_finalizer()
+        self._finalizer = weakref.finalize(self, self._cleanup_files, self.parquet_paths)
 
     @classmethod
     def get_directory(cls) -> Path:
@@ -203,13 +226,21 @@ class ParquetDataTable(Sized):
         Path(parquet_path).parent.mkdir(exist_ok=True, parents=True)
         dataframe.rename(
             columns={column: str(column) for column in dataframe.columns}
-        ).reset_index().to_parquet(parquet_path, index=False)
+        ).reset_index().to_parquet(
+            parquet_path,
+            index=False,
+            row_group_size=PARQUET_ROW_GROUP_SIZE,
+            compression=PARQUET_COMPRESSION,
+        )
 
         index_names = dataframe.index.name or dataframe.index.names
         if len(index_names) == 1 and index_names[0] is None:
             index_names = "index"
 
-        return (Path(parquet_path), cast(Optional[Union[str, Iterable[str]]], index_names))
+        return (
+            Path(parquet_path),
+            cast(Optional[Union[str, Iterable[str]]], index_names),
+        )
 
     @classmethod
     def from_dataframe(
@@ -328,7 +359,11 @@ class ParquetDataTable(Sized):
             relation_hash = h.hexdigest()
 
             new_parquet_path = self.get_directory() / f"{prefix_path}_{relation_hash}.parquet"
-            relation.to_parquet(str(new_parquet_path))
+            relation.to_parquet(
+                str(new_parquet_path),
+                row_group_size=PARQUET_ROW_GROUP_SIZE,
+                compression=PARQUET_COMPRESSION,
+            )
             new_parquet_paths.append(new_parquet_path)
 
         print(f"{new_parquet_paths=}")
@@ -346,8 +381,7 @@ class ParquetDataTable(Sized):
 
     def persist(self) -> None:
         """Disable parquet file removal."""
-        if self._finalizer is not None:
-            self._finalizer.detach()
+        self._detach_finalizer()
 
 
 class GeoDataTable(ParquetDataTable):
@@ -364,6 +398,8 @@ class GeoDataTable(ParquetDataTable):
         parquet_paths: Iterable[Path],
         index_column_names: Optional[Union[str, Iterable[str]]] = None,
         persist_files: bool = False,
+        sort_geometries: bool = True,
+        sort_extent: Optional[tuple[float, float, float, float]] = None,
     ):
         """
         Initialize GeoDataTable.
@@ -373,6 +409,10 @@ class GeoDataTable(ParquetDataTable):
             index_column_names (Optional[Union[str, Iterable[str]]]): Index column names.
             persist_files (bool, optional): Whether to keep the files after object removal or
                 delete them from disk. Defaults to False.
+            sort_geometries (bool, optional): Whether to sort geometries to keep geometries
+                organized. Defaults to True.
+            sort_extent (Optional[tuple[float, float, float, float]], optional): Extent to use
+                in the ST_Hilbert sorting. Defaults to None.
         """
         super().__init__(parquet_paths, index_column_names, persist_files)
 
@@ -385,12 +425,66 @@ class GeoDataTable(ParquetDataTable):
                 f" Expecting geoarrow compatible column named '{GEOMETRY_COLUMN}'."
             )
 
+        if sort_geometries:
+            new_parquet_path = self._sort_geometries(sort_extent)
+            self._detach_finalizer()
+            self._cleanup_files(self.parquet_paths)
+            self.parquet_paths = [new_parquet_path]
+
+            if not self.persist_files:
+                self._configure_finalizer()
+
+    @classmethod
+    def from_parquet(
+        cls,
+        parquet_path: Union[Path, str, Iterable[Union[Path, str]]],
+        index_column_names: Optional[Union[str, Iterable[str]]] = None,
+        persist_files: bool = False,
+        sort_geometries: bool = False,
+        sort_extent: Optional[tuple[float, float, float, float]] = None,
+    ) -> "GeoDataTable":
+        """
+        Create GeoDataTable object from parquet files.
+
+        Args:
+            parquet_path (Union[Path, str, Iterable[Union[Path, str]]]): Path or list of parquet
+                paths.
+            index_column_names (Optional[Union[str, Iterable[str]]]): Index column name or names.
+            persist_files (bool, optional): Whether to keep the files after object removal or
+                delete them from disk. Defaults to False.
+            sort_geometries (bool, optional): Whether to sort geometries to keep geometries
+                organized. Defaults to True.
+            sort_extent (Optional[tuple[float, float, float, float]], optional): Extent to use
+                in the ST_Hilbert sorting. Defaults to None.
+
+        Returns:
+            GeoDataTable: Created object.
+        """
+        if isinstance(parquet_path, (str, Path)):
+            return cls(
+                parquet_paths=[Path(parquet_path)],
+                index_column_names=index_column_names,
+                persist_files=persist_files,
+                sort_geometries=sort_geometries,
+                sort_extent=sort_extent,
+            )
+
+        return cls(
+            parquet_paths=[Path(p) for p in parquet_path],
+            index_column_names=index_column_names,
+            persist_files=persist_files,
+            sort_geometries=sort_geometries,
+            sort_extent=sort_extent,
+        )
+
     @classmethod
     def from_geodataframe(
         cls,
         geodataframe: gpd.GeoDataFrame,
         parquet_path: Optional[Union[Path, str]] = None,
         persist_files: bool = False,
+        sort_geometries: bool = True,
+        sort_extent: Optional[tuple[float, float, float, float]] = None,
     ) -> "GeoDataTable":
         """
         Create GeoDataTable object from GeoDataFrame.
@@ -401,6 +495,10 @@ class GeoDataTable(ParquetDataTable):
                 file. Defaults to None.
             persist_files (bool, optional): Whether to keep the files after object removal or
                 delete them from disk. Defaults to False.
+            sort_geometries (bool, optional): Whether to sort geometries to keep geometries
+                organized. Defaults to True.
+            sort_extent (Optional[tuple[float, float, float, float]], optional): Extent to use
+                in the ST_Hilbert sorting. Defaults to None.
 
         Returns:
             GeoDataTable: Created object.
@@ -411,6 +509,8 @@ class GeoDataTable(ParquetDataTable):
             parquet_path=parquet_path,
             index_column_names=index_names,
             persist_files=persist_files,
+            sort_geometries=sort_geometries,
+            sort_extent=sort_extent,
         )
 
     def to_geodataframe(self) -> gpd.GeoDataFrame:
@@ -419,6 +519,97 @@ class GeoDataTable(ParquetDataTable):
         if self.index_column_names is not None:
             gdf.set_index(self.index_column_names, inplace=True)
         return gdf
+
+    def _sort_geometries(self, sort_extent: Optional[tuple[float, float, float, float]]) -> Path:
+        """Order geometries and save parquet files again."""
+        prefix_path = self.generate_filename()
+
+        # https://medium.com/radiant-earth-insights/using-duckdbs-hilbert-function-with-geop-8ebc9137fb8a
+        if sort_extent is None:
+            order_clause = f"""
+            ST_Hilbert(
+                {GEOMETRY_COLUMN},
+                (
+                    SELECT ST_Extent(ST_Extent_Agg({GEOMETRY_COLUMN}))::BOX_2D
+                    FROM ({self.to_duckdb().sql_query()})
+                )
+            )
+            """
+        else:
+            extent_box_clause = f"""
+            {{
+                min_x: {sort_extent[0]},
+                min_y: {sort_extent[1]},
+                max_x: {sort_extent[2]},
+                max_y: {sort_extent[3]}
+            }}::BOX_2D
+            """
+            order_clause = f"""
+            ST_Within(ST_Extent({GEOMETRY_COLUMN}), {extent_box_clause}),
+            ST_Hilbert(
+                {GEOMETRY_COLUMN},
+                {extent_box_clause}
+            )
+            """
+
+        relation = duckdb.sql(
+            f"""
+            SELECT *
+            FROM ({self.to_duckdb().sql_query()})
+            ORDER BY {order_clause}
+            """
+        )
+
+        print(relation.sql_query())
+
+        h = hashlib.new("sha256")
+        h.update(relation.sql_query().encode())
+        relation_hash = h.hexdigest()
+
+        new_parquet_path = self.get_directory() / f"{prefix_path}_{relation_hash}_sorted.parquet"
+
+        relation.to_parquet(
+            str(new_parquet_path),
+            row_group_size=PARQUET_ROW_GROUP_SIZE,
+            compression=PARQUET_COMPRESSION,
+        )
+
+        return new_parquet_path
+
+    def union_all(self, method: Literal["coverage", "unary"] = "coverage") -> BaseGeometry:
+        """Return union of all geometries in the GeoDataTable."""
+        if method == "coverage":
+            union_fn = coverage_union_all
+        elif method == "unary":
+            union_fn = union_all
+        else:
+            raise ValueError(f"Method '{method}' not recognized. Use 'coverage' or 'unary'.")
+
+        dataset = ds.dataset(self.parquet_paths)
+
+        tuples_to_queue = []
+        for pq_file in dataset.files:
+            for row_group in range(pq.ParquetFile(pq_file).num_row_groups):
+                tuples_to_queue.append((pq_file, row_group))
+
+        no_cpus = multiprocessing.cpu_count()
+
+        total_row_groups = len(tuples_to_queue)
+
+        with ProcessPoolExecutor(max_workers=min(no_cpus, total_row_groups)) as ex:
+            fn = partial(_union_geometries, union_fn=union_fn)
+            geometries = list(
+                tqdm(
+                    ex.map(fn, tuples_to_queue, chunksize=1),
+                    desc="Unioning geometries",
+                    total=total_row_groups,
+                )
+            )
+
+        if len(geometries) == 1:
+            return geometries[0]
+
+        return union_fn(geometries)
 
 
 VALID_GEO_INPUT = Union[Path, str, Iterable[Union[Path, str]], gpd.GeoDataFrame, GeoDataTable]
@@ -443,3 +634,19 @@ def prepare_data_input(data_input: VALID_DATA_INPUT) -> ParquetDataTable:
         return ParquetDataTable.from_dataframe(data_input)
 
     return ParquetDataTable.from_parquet(data_input)
+
+
+def _union_geometries(
+    parquet_info_tuple: tuple[str, int],
+    union_fn: Callable[[Iterable[BaseGeometry]], BaseGeometry],
+) -> BaseGeometry:
+    parquet_path, row_group = parquet_info_tuple
+    return union_fn(
+        to_shapely(
+            ChunkedGeometryArray.from_arrow(
+                pq.ParquetFile(parquet_path).read_row_group(row_group, columns=[GEOMETRY_COLUMN])[
+                    GEOMETRY_COLUMN
+                ]
+            )
+        )
+    )
