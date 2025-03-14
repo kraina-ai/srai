06e72fdc652055d654b4ac121e75fd069e16d52d
Julia Moska <j.moskaaa@gmail.com>
2025-03-14 14:14:01 +0100

    fix: renamed regression evaluator to hex regression evaluator

--- srai/benchmark/__init__.py
+++ srai/benchmark/__init__.py
@@ -1,6 +1,6 @@
 """The benchmark module contains classes for evaluating the performance of a model on a dataset."""
 
 from ._base import BaseEvaluator
-from .regression_evaluator import HexRegressionEvaluator
+from .hex_regression_evaluator import HexRegressionEvaluator
 
 __all__ = ["BaseEvaluator", "HexRegressionEvaluator"]
