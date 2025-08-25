442ac21035bfad738d061f01cb2cba5bdf011db6
Kamil Raczycki <raczyckikamil@gmail.com>
2025-08-25 10:33:24 +0200

    feat: finish contextual count embedder implementation

--- srai/embedders/contextual_count_embedder.py
+++ srai/embedders/contextual_count_embedder.py
@@ -9,7 +9,12 @@ References:
 """
 
 import tempfile
+import warnings
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
 from itertools import product
+from math import ceil
+from multiprocessing import cpu_count
 from pathlib import Path
 from typing import Literal, Optional, Union, cast
 
@@ -21,7 +26,10 @@ from rq_geo_toolkit.constants import (
     PARQUET_ROW_GROUP_SIZE,
 )
 from rq_geo_toolkit.duckdb import run_query_with_memory_monitoring, sql_escape
+from rq_geo_toolkit.multiprocessing_utils import WorkerProcess, run_process_with_memory_monitoring
+from tqdm import tqdm
 
+from srai.constants import FORCE_TERMINAL
 from srai.duckdb import prepare_duckdb_extensions, relation_from_parquet_paths
 from srai.embedders.count_embedder import CountEmbedder
 from srai.geodatatable import VALID_DATA_INPUT, ParquetDataTable
@@ -123,10 +131,6 @@ class ContextualCountEmbedder(CountEmbedder):
         """
         counts_pdt = super().transform(regions, features, joint)
 
-        # TODO: run _iterate_parquet_data in workerprocess
-        # TODO: generate neighbours in multiprocessing
-        # TODO: add progress bar and print error (warnings?)
-
         if counts_pdt.empty:
             if self.concatenate_vectors:
                 empty_df = pd.DataFrame(
@@ -169,208 +173,52 @@ class ContextualCountEmbedder(CountEmbedder):
         result_dir_path.mkdir(parents=True, exist_ok=True)
         saved_result_files = []
 
-        while current_offset < total_rows:
-            try:
-                current_result_file_path = result_dir_path / f"{current_file_idx}.parquet"
-                _parse_single_batch(
-                    neighbourhood=self.neighbourhood,
-                    neighbourhood_distance=self.neighbourhood_distance,
-                    aggregation_function=self.aggregation_function,
-                    concatenate_vectors=self.concatenate_vectors,
-                    counts_parquet_files=list(counts_pdt.parquet_paths),
-                    index_name=cast("str", counts_pdt.index_name),
-                    feature_column_names=counts_pdt.columns,
-                    limit=current_limit,
-                    offset=current_offset,
-                    result_file_path=current_result_file_path,
-                )
-                saved_result_files.append(current_result_file_path)
+        with tqdm(
+            total=total_rows,
+            desc="Generating embeddings for neighbours",
+            disable=FORCE_TERMINAL,
+        ) as pbar:
+            while current_offset < total_rows:
+                try:
+                    current_result_file_path = result_dir_path / f"{current_file_idx}.parquet"
+                    process = WorkerProcess(
+                        target=_parse_single_batch,
+                        kwargs=dict(
+                            neighbourhood=self.neighbourhood,
+                            neighbourhood_distance=self.neighbourhood_distance,
+                            aggregation_function=self.aggregation_function,
+                            concatenate_vectors=self.concatenate_vectors,
+                            counts_parquet_files=list(counts_pdt.parquet_paths),
+                            index_name=cast("str", counts_pdt.index_name),
+                            feature_column_names=counts_pdt.columns,
+                            limit=current_limit,
+                            offset=current_offset,
+                            result_file_path=current_result_file_path,
+                        ),
+                    )
+                    run_process_with_memory_monitoring(process)
 
-                current_file_idx += 1
-                current_offset += current_limit
-                # if progress_callback:
-                #     progress_callback(min(current_offset, total_rows))
+                    saved_result_files.append(current_result_file_path)
 
-            except (duckdb.OutOfMemoryException, MemoryError):
-                current_limit //= 10
-                if current_limit == 1:
-                    raise
+                    current_file_idx += 1
+                    current_offset += current_limit
+                    pbar.n = min(current_offset, total_rows)
+                    pbar.refresh()
 
-                # if not verbosity_mode == "silent":
-                #     rprint(
-                #         f"Encountered {ex.__class__.__name__} during operation."
-                #         f" Retrying with lower number of rows per batch ({current_limit} rows)."
-                #     )
+                except (duckdb.OutOfMemoryException, MemoryError) as ex:
+                    current_limit //= 10
+                    if current_limit == 1:
+                        raise
+
+                    warnings.warn(
+                        f"Encountered {ex.__class__.__name__} during operation."
+                        f" Re trying with lower number of rows per batch ({current_limit} rows).",
+                        stacklevel=1,
+                    )
 
         return ParquetDataTable.from_parquet(
             parquet_path=saved_result_files, index_column_names=counts_pdt.index_name
         )
-
-    # def _get_squashed_embeddings(self, counts_pdt: ParquetDataTable) -> ParquetDataTable:
-    #     """
-    #     Generate embeddings for regions by summing all neighbourhood levels.
-
-    #     Args:
-    #         counts_pdt (ParquetDataTable): Calculated features from CountEmbedder.
-
-    #     Returns:
-    #     """
-    #     counts_df = counts_pdt.to_dataframe()
-    #     base_columns = list(counts_df.columns)
-
-    #     result_array = counts_df.values.astype(float)
-
-    #     for distance, aggregated_values in self._get_aggregated_values_for_distances(counts_df):
-    #         result_array += aggregated_values / ((distance + 1) ** 2)
-
-    #     return pd.DataFrame(data=result_array, index=counts_df.index, columns=base_columns)
-
-    # def _get_concatenated_embeddings(self, counts_pdt: ParquetDataTable) -> ParquetDataTable:
-    #     """
-    #     Generate embeddings for regions by concatenating different neighbourhood levels.
-
-    #     Creates embedding by getting an average of a neighbourhood at a given distance and adding
-    #     those features as separate columns with a postfix added to feature name. This way,
-    #     all neighbourhoods can be analyzed separately, but number of columns grows linearly with
-    #     a distance.
-
-    #     Args:
-    #         counts_pdt (ParquetDataTable): Calculated features from CountEmbedder.
-
-    #     Returns:
-    #         ParquetDataTable: Embedding for each region in regions_gdf with number of features
-    #             equal to a number of features returned by the CountEmbedder multiplied
-    #             by (neighbourhood distance + 1).
-    #     """
-    #     counts_df = counts_pdt.to_dataframe()
-    #     base_columns = list(counts_df.columns)
-    #     number_of_base_columns = len(base_columns)
-    #     columns = [
-    #         f"{column}_{distance}"
-    #         for distance in range(self.neighbourhood_distance + 1)
-    #         for column in base_columns
-    #     ]
-
-    #     result_array = np.zeros(shape=(len(counts_df.index), len(columns)))
-    #     result_array[:, 0:number_of_base_columns] = counts_df.values
-
-    #     for distance, aggregated_values in self._get_aggregated_values_for_distances(counts_df):
-    #         result_array[
-    #             :,
-    #             number_of_base_columns * distance : number_of_base_columns * (distance + 1),
-    #         ] = aggregated_values
-
-    #     return pd.DataFrame(data=result_array, index=counts_df.index, columns=columns)
-
-    # def _get_aggregated_values_for_distances(
-    #     self, counts_df: pd.DataFrame
-    # ) -> Iterator[tuple[int, npt.NDArray[np.float32]]]:
-    #     """
-    #     Generate aggregated values for neighbours at given distances.
-
-    #     Function will yield tuples of distances and aggregated values arrays
-    #     calculated based on neighbours at a given distance.
-
-    #     Distance 0 is skipped.
-    #     If embedder has `neighbourhood_distance` set to 0, nothing will be returned.
-
-    #     Args:
-    #         counts_df (pd.DataFrame): Calculated features from CountEmbedder.
-
-    #     Yields:
-    #         Iterator[Tuple[int, npt.NDArray[np.float32]]]: Iterator of distances and values.
-    #     """
-    #     if self.neighbourhood_distance == 0:
-    #         return
-
-    #     number_of_base_columns = len(counts_df.columns)
-
-    #     activate_multiprocessing = (
-    #         self.num_of_multiprocessing_workers > 1
-    #         and len(counts_df.index) >= self.multiprocessing_activation_threshold
-    #     )
-
-    #     with (
-    #         tqdm(
-    #             total=self.neighbourhood_distance * len(counts_df.index) * 2,
-    #             desc="Generating embeddings for neighbours",
-    #             disable=FORCE_TERMINAL,
-    #         ) as pbar,
-    #         ProcessPoolExecutor(max_workers=self.num_of_multiprocessing_workers) as executor,
-    #     ):
-    #         for distance in range(1, self.neighbourhood_distance + 1):
-    #             pbar.set_postfix_str(f"Distance: {distance}", refresh=True)
-    #             if len(counts_df.index) == 0:
-    #                 continue
-
-    #             neighbours_series = []
-
-    #             if activate_multiprocessing:
-    #                 fn_neighbours = partial(
-    #                     _get_existing_neighbours_at_distance,
-    #                     neighbour_distance=distance,
-    #                     counts_index=counts_df.index,
-    #                     neighbourhood=self.neighbourhood,
-    #                 )
-
-    #                 for result in executor.map(
-    #                     fn_neighbours,
-    #                     counts_df.index,
-    #                     chunksize=ceil(
-    #                         len(counts_df.index) / (4 * self.num_of_multiprocessing_workers)
-    #                     ),
-    #                 ):
-    #                     neighbours_series.append(result)
-    #                     pbar.update()
-    #             else:
-    #                 for result in counts_df.index.map(
-    #                     lambda region_id, neighbour_distance=distance: counts_df.index
-    # .intersection(
-    #                         self.neighbourhood.get_neighbours_at_distance(
-    #                             region_id, neighbour_distance, include_center=False
-    #                         )
-    #                     ).values
-    #                 ):
-    #                     neighbours_series.append(result)
-    #                     pbar.update()
-
-    #             if not neighbours_series:
-    #                 continue
-
-    #             values_to_stack = []
-
-    #             if activate_multiprocessing:
-    #                 fn_embeddings = partial(
-    #                     _get_embeddings_for_neighbours,
-    #                     counts_df=counts_df,
-    #                     aggregation_function=self.aggregation_function,
-    #                     number_of_base_columns=number_of_base_columns,
-    #                 )
-
-    #                 for result in executor.map(
-    #                     fn_embeddings,
-    #                     neighbours_series,
-    #                     chunksize=ceil(
-    #                         len(neighbours_series) / (4 * self.num_of_multiprocessing_workers)
-    #                     ),
-    #                 ):
-    #                     values_to_stack.append(result)
-    #                     pbar.update()
-    #             else:
-    #                 for neighbours in neighbours_series:
-    #                     values_to_stack.append(
-    #                         _get_embeddings_for_neighbours(
-    #                             region_ids=neighbours,
-    #                             counts_df=counts_df,
-    #                             aggregation_function=self.aggregation_function,
-    #                             number_of_base_columns=number_of_base_columns,
-    #                         )
-    #                     )
-    #                     pbar.update()
-
-    #             aggregated_values_stacked = np.stack(values_to_stack)
-
-    #             yield distance, aggregated_values_stacked
 
 
 def _parse_single_batch(
@@ -385,15 +233,16 @@ def _parse_single_batch(
     offset: int,
     result_file_path: Path,
 ) -> None:
+    num_of_multiprocessing_workers = min(1, cpu_count() - 2)
     with (
         tempfile.TemporaryDirectory(dir="files") as tmp_dir_name,
         duckdb.connect(
             database=str(Path(tmp_dir_name) / "db.duckdb"),
             config=dict(preserve_insertion_order=True),
         ) as connection,
+        ProcessPoolExecutor(max_workers=num_of_multiprocessing_workers) as executor,
     ):
         tmp_dir_path = Path(tmp_dir_name)
-        print("_parse_single_batch", limit, offset)
         prepare_duckdb_extensions(conn=connection)
         full_relation = relation_from_parquet_paths(
             parquet_paths=counts_parquet_files, connection=connection, with_row_number=True
@@ -401,16 +250,19 @@ def _parse_single_batch(
         current_batch_relation = full_relation.limit(n=limit, offset=offset)
         current_region_ids = current_batch_relation.select(index_name).fetchdf()[index_name]
 
-        # TODO: make it multiprocessing
-        all_neighbours = set()
-        for region_id in current_region_ids:
-            all_neighbours.add((region_id, region_id, 0))
-        for distance in range(1, neighbourhood_distance + 1):
-            for region_id in current_region_ids:
-                for neighbour_region_id in neighbourhood.get_neighbours_at_distance(
-                    region_id, distance, include_center=False
-                ):
-                    all_neighbours.add((region_id, neighbour_region_id, distance))
+        all_neighbours = set((region_id, region_id, 0) for region_id in current_region_ids)
+
+        fn_neighbours = partial(
+            _get_existing_neighbours_at_distance,
+            neighbourhood=neighbourhood,
+            neighbourhood_distance=neighbourhood_distance,
+        )
+        for result in executor.map(
+            fn_neighbours,
+            current_region_ids,
+            chunksize=ceil(len(current_region_ids) / (4 * num_of_multiprocessing_workers)),
+        ):
+            all_neighbours.update(result)
 
         precalculated_neighbours_path = tmp_dir_path / "neighbours.parquet"
         pd.DataFrame(
@@ -580,3 +432,17 @@ def _generate_concatenated_embeddings_query(
     joined_relation = connection.sql(joined_query)
 
     return joined_relation
+
+
+def _get_existing_neighbours_at_distance(
+    region_id: IndexType,
+    neighbourhood: Neighbourhood[IndexType],
+    neighbourhood_distance: int,
+) -> set[tuple[IndexType, IndexType, int]]:
+    return set(
+        (region_id, neighbour_region_id, distance)
+        for distance in range(1, neighbourhood_distance + 1)
+        for neighbour_region_id in neighbourhood.get_neighbours_at_distance(
+            region_id, distance, include_center=False
+        )
+    )
