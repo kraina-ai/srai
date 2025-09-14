81141cbae0bba9c43ceb4fe01d418c9783c5923e
Kamil Raczycki <raczyckikamil@gmail.com>
2025-09-14 21:07:42 +0200

    fix: raise error in porto taxi if no resolution

--- srai/datasets/porto_taxi.py
+++ srai/datasets/porto_taxi.py
@@ -301,7 +301,10 @@ class PortoTaxiDataset(TrajectoryDataset):
         if version in ("TTE", "HMP"):
             self.resolution = 9
         elif version == "all":
-            self.resolution = resolution if resolution is not None else None
+            if resolution is None:
+                raise ValueError("Pass the resolution parameter to generate h3 trajectories.")
+            else:
+                self.resolution = resolution
         else:
             raise NotImplementedError("Version not implemented")
         return super().load(hf_token=hf_token, version=version)
